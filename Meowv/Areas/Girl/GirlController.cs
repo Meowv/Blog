036adb7e1eea8b1760ec1b2e1f13004667f6ec70
阿星Plus <hackxing@foxmail.com>
2018-07-16 20:02:59 +0800

    随机妹子图API

---
+++ Meowv/Areas/Girl/GirlController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Meowv.Areas.Girl
+{
+    [ApiController, Route("[Controller]")]
+    public class GirlController : ControllerBase
+    {
+        /// <summary>
+        /// 获取随机妹子图
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetGirl()
+        {
+            try
+            {
+                var url = "https://gank.io/api/random/data/福利/1";
+
+                using (var http = new HttpClient())
+                {
+                    var jsonContent = await http.GetStringAsync(url);
+
+                    var obj = JObject.Parse(jsonContent);
+
+                    var girlPicName = obj["results"][0]["url"].ToString();
+
+                    var bytes = await http.GetByteArrayAsync(girlPicName);
+
+                    return File(bytes, "image/jpeg");
+                }
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
