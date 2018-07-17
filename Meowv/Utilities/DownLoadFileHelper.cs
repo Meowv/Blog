e9472f3d23b879475dace0ab5179f096bc960987
阿星Plus <hackxing@foxmail.com>
2018-07-17 16:50:25 +0800

    下载文件通用类

---
+++ Meowv/Utilities/DownLoadFileHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net.Http;
+
+namespace Meowv.Utilities
+{
+    public class DownLoadFileHelper
+    {
+        /// <summary>
+        /// 下载文件工具类
+        /// </summary>
+        /// <param name="url">地址</param>
+        /// <param name="path">路径</param>
+        public static void DownLoadFile(string url, string path)
+        {
+            using (var http = new HttpClient())
+            {
+                var buffer = http.GetByteArrayAsync(url);
+                using (var ms = new MemoryStream(buffer.Result))
+                {
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        var bytes = new byte[1024];
+                        var size = ms.Read(bytes, 0, bytes.Length);
+                        while (size > 0)
+                        {
+                            stream.Write(bytes, 0, size);
+                            size = ms.Read(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
