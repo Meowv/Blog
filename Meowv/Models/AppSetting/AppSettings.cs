3b0e9806e824f94a2736e095acaecfbc70bc447f
阿星Plus <hackxing@foxmail.com>
2018-07-17 11:36:17 +0800

    实体 summary 注释

--- Meowv/Models/AppSetting/AppSettings.cs
+++ Meowv/Models/AppSetting/AppSettings.cs
@@ -2,6 +2,9 @@
 {
     public class AppSettings
     {
+        /// <summary>
+        /// 猫图数量
+        /// </summary>
         public int CatCount { get; set; }
     }
 }
--- Meowv/Models/JsonResult/JsonResult.cs
+++ Meowv/Models/JsonResult/JsonResult.cs
@@ -2,7 +2,14 @@
 {
     public class JsonResult<T>
     {
+        /// <summary>
+        /// 成功/失败
+        /// </summary>
         public string Reason { get; set; } = "success";
+
+        /// <summary>
+        /// 返回结果
+        /// </summary>
         public T Result { get; set; }
     }
 }
