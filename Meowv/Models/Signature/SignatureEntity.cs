5cd376b9bd9c33b1cd339f288f80ff7fceb62486
阿星Plus <hackxing@foxmail.com>
2018-07-17 17:37:53 +0800

    获取艺术签名接口 待完成...

---
+++ Meowv/Models/Signature/SignatureEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Meowv.Models.Signature
+{
+    public class SignatureEntity
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 签名类型
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// 签名地址
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
---
+++ Meowv/Processor/Signature/SignatureEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace Meowv.Processor.Signature
+{
+    /// <summary>
+    /// 签名类型
+    /// </summary>
+    public enum SignatureEnum
+    {
+        /// <summary>
+        /// 艺术签
+        /// </summary>
+        _art = 901,
+
+        /// <summary>
+        /// 商务签
+        /// </summary>
+        _biz = 905
+    }
+}
