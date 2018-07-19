b8e199f77f5b559ad9cb705ee6d0752cc1161849
阿星Plus <hackxing@foxmail.com>
2018-07-19 17:57:15 +0800

    页面title，api.meowv.com

--- Meowv/Controllers/HomeController.cs
+++ Meowv/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Meowv.Controllers
 {
@@ -10,6 +6,7 @@ namespace Meowv.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Title = "api.meowv.com";
             return View();
         }
     }
