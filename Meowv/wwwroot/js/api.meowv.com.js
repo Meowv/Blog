2fd7aa55fb0b137fef4a2cc3f666df275c9e2815
阿星Plus <hackxing@foxmail.com>
2018-07-20 16:57:09 +0800

    优雅的页面JavaScript引用方式搭建，博客列表加载完成

---
+++ Meowv/wwwroot/js/api.meowv.com.js
@@ -0,0 +1,75 @@
+﻿var meowv = function () {
+    var _pageData = {
+
+    };
+
+    var reason = "success";
+
+    return {
+        init: function () {
+            this.pageInit();
+        },
+        method: {
+            loadSignature: function () {
+
+            },
+            loadJobs: function () {
+
+            },
+            loadBlogs: function () {
+                $.getJSON('/blog', function (result) {
+                    $("#blog-content").html(template("blog-template", result));
+                });            },
+            loadNews: function () {
+
+            },
+            loadArticle: function () {
+
+            },
+            loadGirl: function () {
+
+            },
+            loadCat: function () {
+
+            },
+            loadBing: function () {
+
+            }
+        },
+        pageInit: function () {
+            switch (window.location.pathname) {
+                case "/":
+                    this.method.loadSignature();
+                    break;
+                case "/index.html":
+                    this.method.loadSignature();
+                    break;
+                case "/job.html":
+                    this.method.loadJobs();
+                    break;
+                case "/blog.html":
+                    this.method.loadBlogs();
+                    break;
+                case "/news.html":
+                    this.method.loadNews();
+                    break;
+                case "/article.html":
+                    this.method.loadArticle();
+                    break;
+                case "/random-article.html":
+                    this.method.loadArticle();
+                    break;
+                case "/girl.html":
+                    break;
+                case "/cat.html":
+                    break;
+                case "/bing.html":
+                    break;
+            }
+        }
+    };
+}();
+
+$(function () {
+    meowv.init();
+});
