dc8ba18eb6e43af702f237c35a046188ae100b2a
阿星Plus <hackxing@foxmail.com>
2018-08-10 16:53:35 +0800

    通用将byte数组保存为文件方法

--- Meowv/Utilities/FileHelper.cs
+++ Meowv/Utilities/FileHelper.cs
@@ -38,5 +38,36 @@ namespace Meowv.Utilities
                 return false;
             }
         }
+
+        /// <summary>
+        /// 将byte数组保存为文件
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static bool SaveFile(byte[] buffer, string path)
+        {
+            try
+            {
+                using (var ms = new MemoryStream(buffer))
+                {
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        var bytes = new byte[1024];
+                        var size = ms.Read(bytes, 0, bytes.Length);
+                        while (size > 0)
+                        {
+                            stream.Write(bytes, 0, size);
+                            size = ms.Read(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
