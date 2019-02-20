5b9e4951cc7f0326e40d8e1e091a2d99b6343a0e
阿星Plus <hackxing@foxmail.com>
2019-02-20 21:35:45 +0800

    分类和标签接口类定义

---
+++ Meowv.Interface/Blog/ICategory.cs
@@ -0,0 +1,42 @@
+﻿using Meowv.Entity.Blog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meowv.Interface.Blog
+{
+    public interface ICategory
+    {
+        /// <summary>
+        /// 添加分类
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> AddCategory(CategoryEntity entity);
+
+        /// <summary>
+        /// 删除分类
+        /// </summary>
+        /// <param name="categoryId"></param>
+        /// <returns></returns>
+        Task<bool> DeleteCategory(int categoryId);
+
+        /// <summary>
+        /// 更新分类
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> UpdateCategory(CategoryEntity entity);
+
+        /// <summary>
+        /// 获取分类列表
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<CategoryEntity>> GetCategories();
+
+        /// <summary>
+        /// 根据文章ID获取分类列表
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<CategoryEntity>> GetCategoriesByArticleId();
+    }
+}
---
+++ Meowv.Interface/Blog/ITag.cs
@@ -0,0 +1,43 @@
+﻿using Meowv.Entity.Blog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meowv.Interface.Blog
+{
+    public interface ITag
+    {
+        /// <summary>
+        /// 添加标签
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> AddTag(TagEntity entity);
+
+        /// <summary>
+        /// 删除标签
+        /// </summary>
+        /// <param name="tagId"></param>
+        /// <returns></returns>
+        Task<bool> DeleteTag(int tagId);
+
+        /// <summary>
+        /// 更新标签
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> UpdateTag(TagEntity entity);
+
+        /// <summary>
+        /// 获取标签列表
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<TagEntity>> GetTags();
+
+        /// <summary>
+        /// 根据文章ID获取标签列表
+        /// </summary>
+        /// <param name="articleId"></param>
+        /// <returns></returns>
+        Task<IEnumerable<ArticleTagEntity>> GetTags(int articleId);
+    }
+}
