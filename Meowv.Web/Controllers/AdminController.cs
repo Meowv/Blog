db83d3ad653846275686c8d6a2e0015d8cb3a659
阿星Plus <hackxing@foxmail.com>
2019-02-23 19:16:45 +0800

    后台框架

---
+++ Meowv.Web/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meowv.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
