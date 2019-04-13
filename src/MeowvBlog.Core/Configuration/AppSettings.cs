87cba1f4c2dcf8a9d60300f7900ff32933872b1c
阿星Plus <hackxing@foxmail.com>
2019-04-13 23:36:31 +0800

    MeowvBlogCoreMoudle

---
+++ src/MeowvBlog.Core/Configuration/AppSettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MeowvBlog.Core.Configuration
+{
+    /// <summary>
+    /// 配置文件数据
+    /// </summary>
+    public class AppSettings
+    {
+        private static readonly IConfigurationRoot _configuration;
+
+        static AppSettings()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            _configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// MySQL数据库连接字符串
+        /// </summary>
+        public static string MySqlConnectionString => _configuration["MySqlConnectionString"];
+
+        /// <summary>
+        /// SqlServer数据库连接字符串
+        /// </summary>
+        public static string SqlServerConnectionString => _configuration["SqlServerConnectionString"];
+    }
+}
---
+++ src/MeowvBlog.Core/MeowvBlogCoreMoudle.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using UPrime;
+using UPrime.Modules;
+
+namespace MeowvBlog.Core
+{
+    [DependsOn(typeof(UPrimeLeadershipModule))]
+    public class MeowvBlogCoreMoudle : UPrimeModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
