6061a215e91fe940cb34fdc109c033d6227f5205
阿星Plus <hackxing@foxmail.com>
2019-04-13 23:36:45 +0800

    MeowvBlogServicsDtoMoudle

---
+++ src/MeowvBlog.Services.Dto/MeowvBlogServicsDtoMoudle.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using UPrime.AutoMapper;
+using UPrime.Modules;
+
+namespace MeowvBlog.Services.Dto
+{
+    [DependsOn(typeof(UPrimeAutoMapperModule))]
+    public class MeowvBlogServicsDtoMoudle : UPrimeModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
