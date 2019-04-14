6711f78f62a142cef445310f88f523f3df6233ad
阿星Plus <hackxing@foxmail.com>
2019-04-14 15:12:21 +0800

    EF 配置

---
+++ src/MeowvBlog.EntityFramework/MeowvBlogDbContextProvider.cs
@@ -0,0 +1,22 @@
+﻿using UPrime.Dependency;
+using UPrime.Domain.Uow;
+using UPrime.EntityFramework;
+using UPrime.EntityFramework.Uow;
+
+namespace MeowvBlog.EntityFramework
+{
+    public class MeowvBlogDbContextProvider : IDbContextProvider<MeowvBlogDbContext>, ITransientDependency
+    {
+        private readonly ICurrentUnitOfWorkProvider _currentUnitOfWorkProvider;
+
+        public MeowvBlogDbContextProvider(ICurrentUnitOfWorkProvider currentUnitOfWorkProvider)
+        {
+            _currentUnitOfWorkProvider = currentUnitOfWorkProvider;
+        }
+
+        public MeowvBlogDbContext GetDbContext()
+        {
+            return ((EfCoreUnitOfWork)_currentUnitOfWorkProvider).GetDbContext<MeowvBlogDbContext>();
+        }
+    }
+}
