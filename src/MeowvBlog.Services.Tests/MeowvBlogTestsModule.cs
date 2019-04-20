2fc6f5255fa3bd0c4590392825f6a521094509c2
阿星Plus <hackxing@foxmail.com>
2019-04-20 16:20:28 +0800

    UnitTesting

---
+++ src/MeowvBlog.Services.Tests/MeowvBlogTestsModule.cs
@@ -0,0 +1,21 @@
+﻿using MeowvBlog.Core;
+using MeowvBlog.EntityFramework;
+using MeowvBlog.Services.Dto;
+using System.Reflection;
+using UPrime.Modules;
+
+namespace MeowvBlog.Services.Tests
+{
+    [DependsOn(
+        typeof(MeowvBlogCoreModule),
+        typeof(MeowvBlogEntityFrameworkModule),
+        typeof(MeowvBlogServicesModule),
+        typeof(MeowvBlogServicsDtoModule))]
+    public class MeowvBlogTestsModule : UPrimeModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
