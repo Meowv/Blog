d6ab2ca1d89d574391d45934136cc23656d8487b
阿星Plus <hackxing@foxmail.com>
2019-04-28 22:38:36 +0800

    前端分页加载数据

--- src/MeowvBlog.API/wwwroot/js/index.js
+++ src/MeowvBlog.API/wwwroot/js/index.js
@@ -1,16 +1,28 @@
 // ·ÖÒ³²¿·Ö
-var options = {
-    size: "small",    bootstrapMajorVersion: 3,
-    currentPage: 1,
-    numberOfPages: 5,
-    totalPages: 100,
-    pageUrl: function (type, page, current) {
-        if (page == current) return "javascript:;";
-        return location.origin + "/?page=" + page;
-    },
-    tooltipTitles: function () { },
-    onPageClicked: function (e, originalEvent, type, page) {
+var page = {
+    pageIndex: location.search.replace(/([^?&=]+)=([^&]+)/g, (_, __, page) => page).replace("?", "") || 1,
+    pageSize: 10
+};
+var parameter = {
+    url: "/article/getlist?PageIndex=" + page.pageIndex + "&PageSize=" + page.pageSize,
+    callback: function (data) {
+        if (data.isSuccess) {
+            if (data.result.items.length == 0)
+                location.href = "/";
 
+            var options = {
+                size: "small",                bootstrapMajorVersion: 3,
+                currentPage: page.pageIndex,
+                numberOfPages: 5,
+                totalPages: Math.ceil(data.result.totalCount / page.pageSize),
+                pageUrl: function (_, page, current) {
+                    if (page == current) return "javascript:;";
+                    return location.origin + "/?page=" + page;
+                },
+                tooltipTitles: function () { }
+            }
+            $("#page").bootstrapPaginator(options);
+        }
     }
-}
-$("#page").bootstrapPaginator(options);
+};
+_ajax(parameter);
