6bfe87ad896fa61f9a87de71b51bcb8e00051c1a
阿星Plus <hackxing@foxmail.com>
2019-05-01 19:26:57 +0800

    获取一篇文章详细信息输出参数

---
+++ src/MeowvBlog.Services.Dto/Articles/Params/GetArticleOutput.cs
@@ -0,0 +1,27 @@
+﻿using MeowvBlog.Services.Dto.Categories;
+using MeowvBlog.Services.Dto.Tags;
+using System.Collections.Generic;
+
+namespace MeowvBlog.Services.Dto.Articles.Params
+{
+    /// <summary>
+    /// 获取一篇文章详细信息输出参数
+    /// </summary>
+    public class GetArticleOutput
+    {
+        /// <summary>
+        /// 文章
+        /// </summary>
+        public ArticleDto Article { get; set; }
+
+        /// <summary>
+        /// 分类
+        /// </summary>
+        public CategoryDto Category { get; set; }
+
+        /// <summary>
+        /// 标签
+        /// </summary>
+        public IList<TagDto> Tags { get; set; }
+    }
+}
