67d202a6f2620b58208622ec5374d1d40eb999d0
阿星Plus <hackxing@foxmail.com>
2019-05-02 15:46:25 +0800

    分类列表页数据绑定

---
+++ src/MeowvBlog.API/wwwroot/js/taglist.js
@@ -0,0 +1,20 @@
+// 分类列表
+var name = window.location.pathname.replace(/\/|tags|list/g, "");
+var parameter = {
+    url: "/tag/article/query?name=" + name,
+    callback: function (data) {
+        if (data.isSuccess) {
+            data.result[0].tags.forEach(function (i) {
+                if (name == i.displayName) {
+                    $("#current-tag").text("TAG : " + i.tagName);
+                }
+            });
+           
+            var html = template("articles_tmpl", { items: data.result });
+            document.getElementById('articles').outerHTML = html;
+        } else {
+            location.href = "/";
+        }
+    }
+};
+_ajax(parameter);
