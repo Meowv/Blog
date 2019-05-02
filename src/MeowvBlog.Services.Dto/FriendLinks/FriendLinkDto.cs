5e5ae412db8656ba5297e3766d3af7ce0ea01450
阿星Plus <hackxing@foxmail.com>
2019-05-02 20:43:11 +0800

    友情链接列表API

---
+++ src/MeowvBlog.Services.Dto/FriendLinks/FriendLinkDto.cs
@@ -0,0 +1,18 @@
+﻿namespace MeowvBlog.Services.Dto.FriendLinks
+{
+    /// <summary>
+    /// 友情链接传输对象
+    /// </summary>
+    public class FriendLinkDto
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 链接
+        /// </summary>
+        public string LinkUrl { get; set; }
+    }
+}
--- src/MeowvBlog.Services/FriendLinks/Impl/FriendLinkService.cs
+++ src/MeowvBlog.Services/FriendLinks/Impl/FriendLinkService.cs
@@ -1,7 +1,42 @@
-﻿namespace MeowvBlog.Services.FriendLinks.Impl
+﻿using MeowvBlog.Core.Domain.FriendLinks.Repositories;
+using MeowvBlog.Services.Dto.FriendLinks;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UPrime;
+using UPrime.AutoMapper;
+
+namespace MeowvBlog.Services.FriendLinks.Impl
 {
+    /// <summary>
+    /// 友情链接服务接口实现
+    /// </summary>
     public class FriendLinkService : ServiceBase, IFriendLinkService
     {
+        private readonly IFriendLinkRepository _friendLinkRepository;
 
+        public FriendLinkService(IFriendLinkRepository friendLinkRepository)
+        {
+            _friendLinkRepository = friendLinkRepository;
+        }
+
+        /// <summary>
+        /// 友情链接列表
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionOutput<IList<FriendLinkDto>>> GetAsync()
+        {
+            using (var uow = UnitOfWorkManager.Begin())
+            {
+                var output = new ActionOutput<IList<FriendLinkDto>>();
+
+                var query = await _friendLinkRepository.GetAllListAsync();
+
+                await uow.CompleteAsync();
+
+                output.Result = query.MapTo<IList<FriendLinkDto>>();
+
+                return output;
+            }
+        }
     }
 }
