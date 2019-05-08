e964e0b75a6c7b3bc9e0cf41eab98b46d40adff1
阿星Plus <hackxing@foxmail.com>
2019-05-08 23:05:44 +0800

    重新定义框架 v3.0.1

--- src/MeowvBlog.Web/Startup.cs
+++ src/MeowvBlog.Web/Startup.cs
@@ -1,33 +1,17 @@
-﻿using MeowvBlog.Data;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace MeowvBlog.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MeowvBlogDbContext>(options =>
-            {
-                options.UseLazyLoadingProxies()
-                       .UseMySql("Server=localhost;port=3306;User Id=root;Password=584200;Database=meowv_blog", sqlOptions =>
-                       {
-                           sqlOptions.EnableRetryOnFailure(
-                               maxRetryCount: 3,
-                               maxRetryDelay: TimeSpan.FromSeconds(30),
-                               errorNumbersToAdd: null);
-                       });
-            });
+            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
