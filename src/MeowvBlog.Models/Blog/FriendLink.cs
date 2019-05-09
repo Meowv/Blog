f0503f76e09e031ecabf39baf67900e5280cb9c7
阿星Plus <hackxing@foxmail.com>
2019-05-09 22:01:32 +0800

    FriendLink Entity

---
+++ src/MeowvBlog.Models/Blog/FriendLink.cs
@@ -0,0 +1,20 @@
+﻿namespace MeowvBlog.Models.Blog
+{
+    public class FriendLink
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 链接
+        /// </summary>
+        public string LinkUrl { get; set; }
+    }
+}
