9a607d8db78d1bee89bfe44e0d8230d846484179
阿星Plus <hackxing@foxmail.com>
2019-05-09 21:53:37 +0800

    Tag Entity

---
+++ src/MeowvBlog.Models/Blog/Tag.cs
@@ -0,0 +1,20 @@
+﻿namespace MeowvBlog.Models.Blog
+{
+    public class Tag
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 标签名称
+        /// </summary>
+        public string TagName { get; set; }
+
+        /// <summary>
+        /// 展示名称
+        /// </summary>
+        public string DisplayName { get; set; }
+    }
+}
