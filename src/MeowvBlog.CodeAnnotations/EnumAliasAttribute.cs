435d34fd2c0c7b7b35b58a808ff6eb1fbfd7ace9
阿星Plus <hackxing@foxmail.com>
2019-05-10 22:55:38 +0800

    EnumExtensions EnumAliasAttribute

---
+++ src/MeowvBlog.CodeAnnotations/EnumAliasAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeowvBlog.CodeAnnotations
+{
+    public class EnumAliasAttribute : Attribute
+    {
+        public string Alias { get; set; }
+
+        public EnumAliasAttribute(string alias)
+        {
+            Alias = alias;
+        }
+    }
+}
