be49ef23cec9204db14b98af98bd64cd2ce23cbb
阿星Plus <hackxing@foxmail.com>
2019-05-10 23:54:24 +0800

    AutoMapper ??? 还不是很明白其用法，明天研究

---
+++ src/MeowvBlog.AutoMapper/AutoMapExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+public static class AutoMapExtensions
+{
+    public static TDestination MapTo<TDestination>(this object source)
+    {
+        return Mapper.Map<TDestination>(source);
+    }
+
+    public static TDestination MapTo<TSource, TDestination>(this TSource source, TDestination destination)
+    {
+        return Mapper.Map(source, destination);
+    }
+}
