77c896e93581820518f095a53a655fc5a38d40e1
阿星Plus <hackxing@foxmail.com>
2019-05-11 15:35:18 +0800

    AutoMapper 封装扩展

---
+++ src/MeowvBlog.AutoMapper/IAutoMapperConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+
+namespace MeowvBlog.AutoMapper
+{
+    public interface IAutoMapperConfiguration
+    {
+        List<Action<IMapperConfigurationExpression>> Configurators { get; }
+
+        bool UseStaticMapper { get; set; }
+    }
+}
