7c7e48012527958475000fd8d38f849df59f5c98
阿星Plus <hackxing@foxmail.com>
2019-05-11 22:56:20 +0800

    Log4Net_Tests

---
+++ src/MeowvBlog.Core/IO/DirectoryHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace MeowvBlog.Core.IO
+{
+    public static class DirectoryHelper
+    {
+        public static void CreateIfNotExists(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
---
+++ src/MeowvBlog.Core/IO/FileHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace MeowvBlog.Core.IO
+{
+    public static class FileHelper
+    {
+        public static void DeleteIfExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
