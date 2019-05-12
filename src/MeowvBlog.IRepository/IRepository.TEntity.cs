ee48072dd0ef0a5c35f1e10e2c7dc0130e92bf5a
阿星Plus <hackxing@foxmail.com>
2019-05-12 16:02:19 +0800

    repository

---
+++ src/MeowvBlog.IRepository/IRepository.TEntity.cs
@@ -0,0 +1,12 @@
+﻿using MeowvBlog.Entities;
+
+namespace MeowvBlog.IRepository
+{
+    /// <summary>
+    /// IRepository
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IRepository<TEntity> : IRepository<TEntity, int>, IRepository where TEntity : class, IEntity<int>
+    {
+    }
+}
