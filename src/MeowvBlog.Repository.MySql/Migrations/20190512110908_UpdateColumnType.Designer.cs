4e91faf4711ef8bb23cbd18139107f95e5b1c3f2
阿星Plus <hackxing@foxmail.com>
2019-05-12 19:10:21 +0800

    UpdateColumnType

--- src/MeowvBlog.Repository.MySql/Migrations/20190512110908_UpdateColumnType.Designer.cs
+++ src/MeowvBlog.Repository.MySql/Migrations/20190512110908_UpdateColumnType.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MeowvBlog.Repository.MySql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MeowvBlog.Repository.MySql.Migrations
 {
     [DbContext(typeof(MeowvBlogDbContext))]
-    partial class MeowvBlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190512110908_UpdateColumnType")]
+    partial class UpdateColumnType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
