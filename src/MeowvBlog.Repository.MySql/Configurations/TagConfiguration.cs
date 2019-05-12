6c6842df5ef7901ea17cd49590ab33cb2b507fb8
阿星Plus <hackxing@foxmail.com>
2019-05-12 19:07:55 +0800

    IsFixedLength

--- src/MeowvBlog.Repository.MySql/Configurations/TagConfiguration.cs
+++ src/MeowvBlog.Repository.MySql/Configurations/TagConfiguration.cs
@@ -11,9 +11,9 @@ namespace MeowvBlog.Repository.MySql.Configurations
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd();
 
-            builder.Property(x => x.TagName).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.TagName).HasMaxLength(50).IsFixedLength().IsRequired();
 
-            builder.Property(x => x.DisplayName).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.DisplayName).HasMaxLength(50).IsFixedLength().IsRequired();
 
             builder.ToTable(DbConsts.DbTableName.Tags);
         }
