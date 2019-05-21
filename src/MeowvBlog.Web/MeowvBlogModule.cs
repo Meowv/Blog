72df295a69aa9f61b508df023d5ae72c47a8e064
阿星Plus <hackxing@foxmail.com>
2019-05-21 20:14:24 +0800

    1

---
+++ src/MeowvBlog.Web/MeowvBlogModule.cs
@@ -0,0 +1,13 @@
+﻿using Plus.Modules;
+using System.Reflection;
+
+namespace MeowvBlog.Web
+{
+    public class MeowvBlogModule : PlusModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
