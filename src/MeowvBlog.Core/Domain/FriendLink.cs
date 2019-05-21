cf3bbcdc06657c2aaaa0a825d572722a0191c3d2
阿星Plus <hackxing@foxmail.com>
2019-05-21 23:23:58 +0800

    entity

---
+++ src/MeowvBlog.Core/Domain/FriendLink.cs
@@ -0,0 +1,17 @@
+﻿using Plus.Domain.Entities;
+
+namespace MeowvBlog.Core.Domain
+{
+    public class FriendLink : Entity
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 链接
+        /// </summary>
+        public string LinkUrl { get; set; }
+    }
+}
---
+++ src/MeowvBlog.EntityFrameworkCore/Mappers/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MeowvBlog.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MeowvBlog.EntityFrameworkCore.Mappers
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            builder.Property(x => x.CategoryName).HasMaxLength(50).IsFixedLength().IsRequired();
+
+            builder.Property(x => x.DisplayName).HasMaxLength(50).IsFixedLength().IsRequired();
+
+            builder.ToTable(DbConsts.DbTableName.Categories);
+        }
+    }
+}
