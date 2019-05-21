a63cec21a7c8522c1b278118bcd75f859cd977e4
阿星Plus <hackxing@foxmail.com>
2019-05-21 23:05:39 +0800

    基于Plus框架的模块配置

---
+++ src/MeowvBlog.Core/Configuration/AppSettings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MeowvBlog.Core.Configuration
+{
+    public class AppSettings
+    {
+        private static readonly IConfigurationRoot _config;
+
+        static AppSettings()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                    .AddJsonFile("appsettings.json", true, true);
+
+            _config = builder.Build();
+        }
+
+        /// <summary>
+        /// MySql
+        /// </summary>
+        public static string MySqlConnectionString => _config["ConnectionStrings:MySql"];
+    }
+}
