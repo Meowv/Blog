c87a13585b03ba7efc9b6a55a885f621e7f8ce29
阿星Plus <hackxing@foxmail.com>
2019-07-24 16:53:10 +0800

    首页搭建

---
+++ src/MeowvBlog.Web/wwwroot/js/main.js
@@ -0,0 +1,100 @@
+// declaraction of document.ready() function.
+(function() {
+    var ie = !!(window.attachEvent && !window.opera);
+    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
+    var fn = [];
+    var run = function() {
+        for (var i = 0; i < fn.length; i++) fn[i]();
+    };
+    var d = document;
+    d.ready = function(f) {
+        if (!ie && !wk && d.addEventListener)
+            return d.addEventListener('DOMContentLoaded', f, false);
+        if (fn.push(f) > 1) return;
+        if (ie)
+            (function() {
+                try {
+                    d.documentElement.doScroll('left');
+                    run();
+                } catch (err) {
+                    setTimeout(arguments.callee, 0);
+                }
+            })();
+        else if (wk)
+            var t = setInterval(function() {
+                if (/^(loaded|complete)$/.test(d.readyState))
+                    clearInterval(t), run();
+            }, 0);
+    };
+})();
+
+
+document.ready(
+    // toggleTheme function.
+    function() {
+        var _Blog = window._Blog || {};
+        const currentTheme = window.localStorage && window.localStorage.getItem('theme');
+        const isDark = currentTheme === 'dark';
+        if (isDark) {
+            document.getElementById("switch_default").checked = true;
+            // mobile
+            document.getElementById("mobile-toggle-theme").innerText = " · Dark"
+        } else {
+            document.getElementById("switch_default").checked = false;
+            // mobile
+            document.getElementById("mobile-toggle-theme").innerText = " · Dark"
+        }
+        _Blog.toggleTheme = function() {
+            if (isDark) {
+                document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                // mobile
+                document.getElementById("mobile-toggle-theme").innerText = " · Dark"
+            } else {
+                document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                // mobile
+                document.getElementById("mobile-toggle-theme").innerText = " · Light"
+            }
+            document.getElementsByClassName('toggleBtn')[0].addEventListener('click', () => {
+                    if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
+                        document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                    } else {
+                        document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                    }
+                    window.localStorage &&
+                        window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light', )
+                })
+                // moblie
+            document.getElementById('mobile-toggle-theme').addEventListener('click', () => {
+                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
+                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+                    // mobile
+                    document.getElementById("mobile-toggle-theme").innerText = " · Light"
+
+                } else {
+                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
+                    // mobile
+                    document.getElementById("mobile-toggle-theme").innerText = " · Dark"
+                }
+                window.localStorage &&
+                    window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light', )
+            })
+        };
+        _Blog.toggleTheme();
+
+        // ready function.
+
+    }
+);
+
+// mobile menu click event
+function mobileBtn() {
+    var toggleMenu = document.getElementsByClassName("menu-toggle")[0];
+    var mobileMenu = document.getElementById("mobile-menu");
+    if (toggleMenu.classList.contains("active")) {
+        toggleMenu.classList.remove("active")
+        mobileMenu.classList.remove("active")
+    } else {
+        toggleMenu.classList.add("active")
+        mobileMenu.classList.add("active")
+    }
+}
