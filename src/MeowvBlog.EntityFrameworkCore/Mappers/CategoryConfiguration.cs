7340122d6e7538d590144a8e7ab6fe545d31677c
阿星Plus <hackxing@foxmail.com>
2019-07-25 13:53:35 +0800

    configuration

--- src/MeowvBlog.EntityFrameworkCore/Mappers/CategoryConfiguration.cs
+++ src/MeowvBlog.EntityFrameworkCore/Mappers/CategoryConfiguration.cs
@@ -10,11 +10,6 @@ namespace MeowvBlog.EntityFrameworkCore.Mappers
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd();
-
-            builder.Property(x => x.CategoryName).HasMaxLength(50).IsFixedLength().IsRequired();
-
-            builder.Property(x => x.DisplayName).HasMaxLength(50).IsFixedLength().IsRequired();
-
             builder.ToTable(DbConsts.DbTableName.Categories);
         }
     }
