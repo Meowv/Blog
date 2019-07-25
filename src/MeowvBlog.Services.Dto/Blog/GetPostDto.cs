f1cfa3594c6961c22fc38ec3bff372dd9fd5d5b8
阿星Plus <hackxing@foxmail.com>
2019-07-25 15:45:43 +0800

    获取文章详情数据重构

--- src/MeowvBlog.Services.Dto/Blog/GetPostDto.cs
+++ src/MeowvBlog.Services.Dto/Blog/GetPostDto.cs
@@ -1,17 +1,55 @@
 ﻿using MeowvBlog.Core.Domain.Blog;
 using Plus.AutoMapper;
+using System.Collections.Generic;
 
 namespace MeowvBlog.Services.Dto.Blog
 {
-    /// <summary>
-    /// 获取文章传输对象
-    /// </summary>
     [AutoMapFrom(typeof(Post))]
-    public class GetPostDto : PostDto
+    public class GetPostDto
     {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// 链接
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+
         /// <summary>
         /// 创建时间
         /// </summary>
-        public new string CreationTime { get; set; }
+        public string CreationTime { get; set; }
+
+        /// <summary>
+        /// 分类
+        /// </summary>
+        public CategoryDto Category { get; set; }
+
+        /// <summary>
+        /// 标签列表
+        /// </summary>
+        public IList<TagDto> Tags { get; set; }
+
+        /// <summary>
+        /// 上一篇
+        /// </summary>
+        public PostForPagedDto Previous { get; set; }
+
+        /// <summary>
+        /// 下一篇
+        /// </summary>
+        public PostForPagedDto Next { get; set; }
     }
 }
