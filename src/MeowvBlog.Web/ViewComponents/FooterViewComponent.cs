ad94c0606a278722cc3edafc321d9634ac22b7c5
阿星Plus <hackxing@foxmail.com>
2019-07-25 17:23:47 +0800

    View Component

---
+++ src/MeowvBlog.Web/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowvBlog.Web.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(bool show = true)
+        {
+            return View("Footer", show);
+        }
+    }
+}
