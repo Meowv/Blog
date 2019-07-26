c0e9d3e826394313e937d78aa2bf9fd913676ef5
阿星Plus <hackxing@foxmail.com>
2019-07-26 23:06:57 +0800

    文章管理、分类管理、标签管理 页面创建

---
+++ src/MeowvBlog.Web/Controllers/Admin/ManageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowvBlog.Web.Controllers.Admin
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ManageController : Controller
+    {
+        [Route("/admin")]
+        public IActionResult Index() => View();
+
+        [Route("/admin/posts")]
+        [Route("/admin/posts/page/{page:int}")]
+        public IActionResult Posts() => View();
+
+        [Route("/admin/categories")]
+        public IActionResult Categories() => View();
+
+        [Route("/admin/tags")]
+        public IActionResult Tags() => View();
+    }
+}
