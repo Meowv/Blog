6619ae92a6eec3c931ad3f308f9ac3a31aa7a26e
阿星Plus <hackxing@foxmail.com>
2019-07-31 18:00:34 +0800

    azuread 线上不生效，改用github

---
+++ src/MeowvBlog.Authorization/GitHub/AccessTokenRequest.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeowvBlog.Authorization.GitHub
+{
+    public class AccessTokenRequest
+    {
+        /// <summary>
+        /// Client ID
+        /// </summary>
+        [Required]
+        public string Client_ID = GitHubConfig.Client_ID;
+
+        /// <summary>
+        /// Client Secret
+        /// </summary>
+        [Required]
+        public string Client_Secret = GitHubConfig.Client_Secret;
+
+        /// <summary>
+        /// 调用API_Authorize获取到的Code值
+        /// </summary>
+        [Required]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Authorization callback URL
+        /// </summary>
+        [Required]
+        public string Redirect_Uri = GitHubConfig.Redirect_Uri;
+
+        /// <summary>
+        /// State
+        /// </summary>
+        public string State { get; set; }
+    }
+}
