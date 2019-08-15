5fbcd09f2861eadb4ed80d4233e86263edeb791b
阿星Plus <hackxing@foxmail.com>
2019-08-15 21:44:59 +0800

    Commits记录获取接口

---
+++ src/MeowvBlog.Services.Dto/Apps/CommitsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MeowvBlog.Services.Dto.Apps
+{
+    public class CommitsDto
+    {
+        public string Sha { get; set; }
+
+        public string Message { get; set; }
+
+        public string Date { get; set; }
+    }
+}
