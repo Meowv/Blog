282b705d54d97007599fcca072c1bc552150d622
阿星Plus <hackxing@foxmail.com>
2019-10-16 15:32:52 +0800

    Swagger扩展，一键调用

---
+++ src/MeowvBlog.Swagger/SwaggerExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+
+namespace MeowvBlog.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            return services.AddSwaggerGen(options =>
+            {
+                var info = new OpenApiInfo
+                {
+                    Version = "v3.1.0",
+                    Title = "阿星Plus - 个人博客以及通用接口"
+                };
+                options.SwaggerDoc("v1", info);
+                options.DocumentFilter<SwaggerDocumentFilter>();
+
+                options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+                {
+                    Description = "JWT模式授权，请输入 Bearer {Token} 进行身份验证",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "MeowvBlog.Web.xml"));
+            });
+        }
+
+        public static void UseSwaggerUI(this IApplicationBuilder app)
+        {
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "个人博客以及通用接口");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
