e747ba18c119771e0c57a72004cea2de7d899c19
阿星Plus <hackxing@foxmail.com>
2019-10-16 19:59:33 +0800

    配置文件

---
+++ src/MeowvBlog.Core/Configurations/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MeowvBlog.Core.Configurations
+{
+    public class AppSettings
+    {
+        private static readonly IConfigurationRoot _config;
+
+        static AppSettings()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                    .AddJsonFile("appsettings.json", true, true);
+            _config = builder.Build();
+        }
+
+        /// <summary>
+        /// Sqlite 连接字符串
+        /// </summary>
+        public static string SqliteConnectionString => _config["SqliteConnectionString"];
+    }
+}
