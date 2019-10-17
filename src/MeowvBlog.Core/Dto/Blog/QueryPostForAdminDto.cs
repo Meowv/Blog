7f7ab22289e0f0c69fbe6d49445d3be1c064c1e8
阿星Plus <hackxing@foxmail.com>
2019-10-17 21:02:27 +0800

    后台管理 分页查询文章列表

---
+++ src/MeowvBlog.Core/Dto/Blog/QueryPostForAdminDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MeowvBlog.Core.Dto.Blog
+{
+    public class QueryPostForAdminDto : QueryPostDto
+    {
+        public new IList<PostBriefForAdminDto> Posts { get; set; }
+    }
+}
