cd45492983d1fc021ea4950e451d98f6c208f8ea
阿星Plus <hackxing@foxmail.com>
2019-10-17 14:27:57 +0800

    SwaggerExtensions 批量添加分组设置

---
+++ src/MeowvBlog.API/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MeowvBlog.API.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        public static List<SwaggerApiInfo> ApiInfos = new List<SwaggerApiInfo>()
+        {
+            new SwaggerApiInfo
+            {
+                UrlPrefix = "v1",
+                Name = "博客前台接口",
+                OpenApiInfo = new OpenApiInfo
+                {
+                    Version = "v3.1.0",
+                    Title = "阿星Plus - 博客前台接口"
+                }
+            },
+            new SwaggerApiInfo
+            {
+                UrlPrefix = "v2",
+                Name = "博客后台接口",
+                OpenApiInfo = new OpenApiInfo
+                {
+                    Version = "v3.1.0",
+                    Title = "阿星Plus - 博客后台接口"
+                }
+            },
+            new SwaggerApiInfo
+            {
+                UrlPrefix = "v3",
+                Name = "其他通用接口",
+                OpenApiInfo = new OpenApiInfo
+                {
+                    Version = "v3.1.0",
+                    Title = "阿星Plus - 其他通用接口"
+                }
+            }
+        };
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            return services.AddSwaggerGen(options =>
+            {
+                ApiInfos.ForEach(x =>
+                {
+                    options.SwaggerDoc(x.UrlPrefix, x.OpenApiInfo);
+                });
+
+                options.DocumentFilter<SwaggerDocumentFilter>();
+
+                options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+                {
+                    Description = "JWT模式授权，请输入 Bearer {Token} 进行身份验证",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "MeowvBlog.API.xml"));
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "MeowvBlog.Core.xml"));
+            });
+        }
+
+        public static void UseSwaggerUI(this IApplicationBuilder app)
+        {
+            app.UseSwaggerUI(options =>
+            {
+                ApiInfos.ForEach(x =>
+                {
+                    options.SwaggerEndpoint($"/swagger/{x.UrlPrefix}/swagger.json", x.Name);
+                });
+                options.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
