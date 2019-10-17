987f50ff12355b50c4f14d24191461929125b0d0
阿星Plus <hackxing@foxmail.com>
2019-10-17 16:14:59 +0800

    分类

--- src/MeowvBlog.API/Controllers/BlogController.cs
+++ src/MeowvBlog.API/Controllers/BlogController.cs
@@ -113,28 +113,31 @@ namespace MeowvBlog.API.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("post/query_by_tag")]
-        public async Task<IList<QueryPostDto>> QueryPostsByTagAsync(string name)
+        public async Task<Response<IList<QueryPostDto>>> QueryPostsByTagAsync(string name)
         {
-            return (from post_tags in await _context.PostTags.ToListAsync()
-                    join tags in await _context.Tags.ToListAsync()
-                    on post_tags.TagId equals tags.Id
-                    join posts in await _context.Posts.ToListAsync()
-                    on post_tags.PostId equals posts.Id
-                    where tags.DisplayName.Equals(name)
-                    orderby posts.CreationTime descending
-                    select new PostBriefDto
-                    {
-                        Title = posts.Title,
-                        Url = posts.Url,
-                        CreationTime = posts.CreationTime.ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-us")),
-                        Year = posts.CreationTime.Year
-                    })
-                    .GroupBy(x => x.Year)
-                    .Select(x => new QueryPostDto
-                    {
-                        Year = x.Key,
-                        Posts = x.ToList()
-                    }).ToList();
+            return new Response<IList<QueryPostDto>>
+            {
+                Result = (from post_tags in await _context.PostTags.ToListAsync()
+                          join tags in await _context.Tags.ToListAsync()
+                          on post_tags.TagId equals tags.Id
+                          join posts in await _context.Posts.ToListAsync()
+                          on post_tags.PostId equals posts.Id
+                          where tags.DisplayName.Equals(name)
+                          orderby posts.CreationTime descending
+                          select new PostBriefDto
+                          {
+                              Title = posts.Title,
+                              Url = posts.Url,
+                              CreationTime = posts.CreationTime.ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-us")),
+                              Year = posts.CreationTime.Year
+                          })
+                          .GroupBy(x => x.Year)
+                          .Select(x => new QueryPostDto
+                          {
+                              Year = x.Key,
+                              Posts = x.ToList()
+                          }).ToList()
+            };
         }
 
         /// <summary>
@@ -144,28 +147,115 @@ namespace MeowvBlog.API.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("post/query_by_category")]
-        public async Task<IList<QueryPostDto>> QueryPostsByCategoryAsync(string name)
+        public async Task<Response<IList<QueryPostDto>>> QueryPostsByCategoryAsync(string name)
         {
-            return (from posts in await _context.Posts.ToListAsync()
-                    join categories in await _context.Categories.ToListAsync()
-                    on posts.CategoryId equals categories.Id
-                    where categories.DisplayName.Equals(name)
-                    orderby posts.CreationTime descending
-                    select new PostBriefDto
-                    {
-                        Title = posts.Title,
-                        Url = posts.Url,
-                        CreationTime = posts.CreationTime.ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-us")),
-                        Year = posts.CreationTime.Year
-                    })
-                    .GroupBy(x => x.Year)
-                    .Select(x => new QueryPostDto
-                    {
-                        Year = x.Key,
-                        Posts = x.ToList()
-                    }).ToList();
+            return new Response<IList<QueryPostDto>>
+            {
+                Result = (from posts in await _context.Posts.ToListAsync()
+                          join categories in await _context.Categories.ToListAsync()
+                          on posts.CategoryId equals categories.Id
+                          where categories.DisplayName.Equals(name)
+                          orderby posts.CreationTime descending
+                          select new PostBriefDto
+                          {
+                              Title = posts.Title,
+                              Url = posts.Url,
+                              CreationTime = posts.CreationTime.ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-us")),
+                              Year = posts.CreationTime.Year
+                          })
+                          .GroupBy(x => x.Year)
+                          .Select(x => new QueryPostDto
+                          {
+                              Year = x.Key,
+                              Posts = x.ToList()
+                          }).ToList()
+            };
         }
 
         #endregion Posts
+
+        #region Tags
+
+        /// <summary>
+        /// 查询标签列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("tags")]
+        public async Task<Response<IList<QueryTagDto>>> QueryTagsAsync()
+        {
+            return new Response<IList<QueryTagDto>>
+            {
+                Result = (from tags in await _context.Tags.ToListAsync()
+                          join post_tags in await _context.PostTags.ToListAsync()
+                          on tags.Id equals post_tags.TagId
+                          group tags by new
+                          {
+                              tags.TagName,
+                              tags.DisplayName
+                          } into g
+                          select new QueryTagDto
+                          {
+                              TagName = g.Key.TagName,
+                              DisplayName = g.Key.DisplayName,
+                              Count = g.Count()
+                          }).ToList()
+            };
+        }
+
+        #endregion Tags
+
+        #region Categories
+
+        /// <summary>
+        /// 获取分类名称
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("category")]
+        public async Task<Response<string>> GetCategory(string name)
+        {
+            var response = new Response<string>();
+
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.DisplayName.Equals(name));
+            if (null == category)
+            {
+                response.Msg = $"分类：{name} 不存在";
+                return response;
+            }
+
+            response.Result = category.CategoryName;
+            return response;
+        }
+
+        /// <summary>
+        /// 查询分类列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("categories")]
+        public async Task<Response<IList<QueryCategoryDto>>> QueryCategoriesAsync()
+        {
+            return new Response<IList<QueryCategoryDto>>
+            {
+                Result = (from category in await _context.Categories.ToListAsync()
+                          join posts in await _context.Posts.ToListAsync()
+                          on category.Id equals posts.CategoryId
+                          group category by new
+                          {
+                              category.CategoryName,
+                              category.DisplayName
+                          } into g
+                          select new QueryCategoryDto
+                          {
+                              CategoryName = g.Key.CategoryName,
+                              DisplayName = g.Key.DisplayName,
+                              Count = g.Count()
+                          }).ToList()
+            };
+        }
+
+        #endregion Categories
     }
 }
