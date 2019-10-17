5423ff50e42711ae2362453a62e20d2e663fc8b2
阿星Plus <hackxing@foxmail.com>
2019-10-17 16:14:59 +0800

    分类

---
+++ src/MeowvBlog.Core/Dto/Blog/QueryCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MeowvBlog.Core.Dto.Blog
+{
+    public class QueryCategoryDto : CategoryDto
+    {
+        public int Count { get; set; }
+    }
+}
---
+++ src/MeowvBlog.Core/Dto/Blog/QueryTagDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MeowvBlog.Core.Dto.Blog
+{
+    public class QueryTagDto : TagDto
+    {
+        public int Count { get; set; }
+    }
+}
