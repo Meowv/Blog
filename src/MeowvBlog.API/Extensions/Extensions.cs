7eaf8d421dbb87bca44ab34fe7a4e2cbf82d9d4d
阿星Plus <hackxing@foxmail.com>
2019-10-21 17:10:49 +0800

    优化 Extensions

--- src/MeowvBlog.API/Extensions/Extensions.cs
+++ src/MeowvBlog.API/Extensions/Extensions.cs
@@ -9,20 +9,14 @@ namespace MeowvBlog.API.Extensions
     {
         public static async Task<T> GetObjFromJsonFile<T>(this string filePath, string key = "") where T : new()
         {
-            if (!File.Exists(filePath))
-            {
-                return new T();
-            }
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                var json = await reader.ReadToEndAsync();
-                if (string.IsNullOrEmpty(key))
-                {
-                    return JsonConvert.DeserializeObject<T>(json);
-                }
+            if (!File.Exists(filePath)) return new T();
 
-                return !(JsonConvert.DeserializeObject<object>(json) is JObject obj) ? new T() : JsonConvert.DeserializeObject<T>(obj[key].ToString());
-            }
+            using StreamReader reader = new StreamReader(filePath);
+            var json = await reader.ReadToEndAsync();
+
+            if (string.IsNullOrEmpty(key)) return JsonConvert.DeserializeObject<T>(json);
+
+            return !(JsonConvert.DeserializeObject<object>(json) is JObject obj) ? new T() : JsonConvert.DeserializeObject<T>(obj[key].ToString());
         }
     }
 }
