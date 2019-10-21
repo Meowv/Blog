dffbd42a7d398c9d1a5edf5fc18453eb7fb656d7
阿星Plus <hackxing@foxmail.com>
2019-10-21 17:06:25 +0800

    随机一张猫图

---
+++ src/MeowvBlog.API/Extensions/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MeowvBlog.API.Extensions
+{
+    public static class Extensions
+    {
+        public static async Task<T> GetObjFromJsonFile<T>(this string filePath, string key = "") where T : new()
+        {
+            if (!File.Exists(filePath))
+            {
+                return new T();
+            }
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                var json = await reader.ReadToEndAsync();
+                if (string.IsNullOrEmpty(key))
+                {
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+
+                return !(JsonConvert.DeserializeObject<object>(json) is JObject obj) ? new T() : JsonConvert.DeserializeObject<T>(obj[key].ToString());
+            }
+        }
+    }
+}
