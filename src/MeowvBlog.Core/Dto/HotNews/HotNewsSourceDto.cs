b4506b882b9df0838994682f3eac232131e88c33
阿星Plus <hackxing@foxmail.com>
2019-10-23 21:13:50 +0800

    获取每日热点来源列表

---
+++ src/MeowvBlog.Core/Dto/HotNews/HotNewsSourceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MeowvBlog.Core.Dto.HotNews
+{
+    public class HotNewsSourceDto
+    {
+        public string Key { get; set; }
+
+        public int Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
