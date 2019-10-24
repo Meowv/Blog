0ce3969ff968510637101a4b9dbb1ac319ec4927
阿星Plus <hackxing@foxmail.com>
2019-10-24 10:36:37 +0800

    优化代码和数据结构

--- src/MeowvBlog.Core/Domain/HotNews/HotNews.cs
+++ src/MeowvBlog.Core/Domain/HotNews/HotNews.cs
@@ -4,7 +4,7 @@ namespace MeowvBlog.Core.Domain.HotNews
 {
     public class HotNews
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string Title { get; set; }
 
