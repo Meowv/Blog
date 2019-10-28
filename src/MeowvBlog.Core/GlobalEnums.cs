1938a15d2736bf693140d61e9b15a5fe94af5bc4
阿星Plus <hackxing@foxmail.com>
2019-10-28 16:02:50 +0800

    跑跑卡丁车手游赛车数据

--- src/MeowvBlog.Core/GlobalEnums.cs
+++ src/MeowvBlog.Core/GlobalEnums.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel;
 
-namespace MeowvBlog.Core.Domain.Signature
+namespace MeowvBlog.Core
 {
     public enum SignatureEnum
     {
@@ -372,5 +372,77 @@ namespace MeowvBlog.Core.Domain.Signature
 
         [Description("马卡龙")]
         马卡龙 = 383,
+    }
+
+    public enum HotNewsSource
+    {
+        [Description("博客园")]
+        cnblogs = 1,
+
+        [Description("V2EX")]
+        v2ex = 2,
+
+        [Description("SegmentFault")]
+        segmentfault = 3,
+
+        [Description("掘金")]
+        juejin = 4,
+
+        [Description("微信热门")]
+        weixin = 5,
+
+        [Description("豆瓣精选")]
+        douban = 6,
+
+        [Description("IT之家")]
+        ithome = 7,
+
+        [Description("36氪")]
+        kr36 = 8,
+
+        [Description("百度贴吧")]
+        tieba = 9,
+
+        [Description("百度热搜")]
+        baidu = 10,
+
+        [Description("微博热搜")]
+        weibo = 11,
+
+        [Description("知乎热榜")]
+        zhihu = 12,
+
+        [Description("知乎日报")]
+        zhihudaily = 13,
+
+        [Description("网易新闻")]
+        news163 = 14,
+
+        [Description("GitHub")]
+        github = 15
+    }
+
+    public enum PopKartType
+    {
+        [Description("竞速")]
+        竞速 = 0,
+
+        [Description("道具")]
+        道具 = 1
+    }
+
+    public enum PopKartRarity
+    {
+        [Description("传说")]
+        传说 = 0,
+
+        [Description("史诗")]
+        史诗 = 1,
+
+        [Description("稀有")]
+        稀有 = 2,
+
+        [Description("普通")]
+        普通 = 3
     }
 }
