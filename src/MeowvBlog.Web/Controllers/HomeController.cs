f7fa8cd6da7fe6d5dc15f69e2fc734cadf53d771
阿星Plus <hackxing@foxmail.com>
2019-10-30 15:37:33 +0800

    asp.net core 3.0 web

---
+++ src/MeowvBlog.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowvBlog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        [Route("/")]
+        [Route("/index.html")]
+        public IActionResult Index() => View();
+    }
+}
