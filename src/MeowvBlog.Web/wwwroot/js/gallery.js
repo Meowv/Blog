4bdeeeed38895297116b9af7a1b4c6d1454a794e
阿星Plus <hackxing@foxmail.com>
2019-11-04 16:56:00 +0800

    优化js

--- src/MeowvBlog.Web/wwwroot/js/gallery.js
+++ src/MeowvBlog.Web/wwwroot/js/gallery.js
@@ -1,19 +1,10 @@
-﻿const api_domain = "https://api.meowv.com";
-var con = document.getElementsByClassName('container');
-var margin = 10;
-var boxes;
-var boxWidth;
-
-axios.get(`${api_domain}/gallery`)
+﻿axios.get(`https://api.meowv.com/gallery`)
     .then(function (response) {
         if (response.data.success) {
             var html = template("gallery_tmpl", response.data);
             document.querySelector('.container').innerHTML = html;
         }
     }).then(function () {
-        boxes = document.getElementsByClassName('albums');
-        boxWidth = boxes[0].offsetWidth + margin;
-
         document.getElementsByClassName = function (className, tag, elm) {
             var testClass = new RegExp("(^|\s)" + className + "(\s|$)");
             var tag = tag || "*";
@@ -31,14 +22,55 @@ axios.get(`${api_domain}/gallery`)
             return returnElements;
         }
 
+        var con = document.getElementsByClassName('container');
+        var margin = 10;
+        var boxes = document.getElementsByClassName('albums');
+        var boxWidth = boxes[0].offsetWidth + margin;
+
+        var show = function () {
+            var columnHeight = [];
+            var bodyWidth = document.body.offsetWidth;
+            var n = parseInt(bodyWidth / boxWidth);
+            var columnNum = n == 0 ? 1 : n;
+            var bodyLeft = bodyWidth >= boxWidth ? bodyWidth - columnNum * boxWidth : 0;
+            con[0].style.left = parseInt(bodyLeft / 2) - margin / 2 + 'px';
+            for (var i = 0, l = boxes.length; i < l; i++) {
+                if (i < columnNum) {
+                    columnHeight[i] = boxes[i].offsetHeight + margin;
+                    boxes[i].style.top = 0;
+                    boxes[i].style.left = i * boxWidth + margin + 'px';
+                } else {
+                    var innsertColumn = min(columnHeight),
+                        imgHeight = boxes[i].offsetHeight + margin;
+                    boxes[i].style.top = columnHeight[innsertColumn] + 'px';
+                    boxes[i].style.left = innsertColumn * boxWidth + margin + 'px';
+                    columnHeight[innsertColumn] += imgHeight;
+                };
+            };
+        }
+
+        var min = function (heightAarry) {
+            var minColumn = 0;
+            var minHeight = heightAarry[minColumn];
+            for (var i = 1, len = heightAarry.length; i < len; i++) {
+                var temp = heightAarry[i];
+                if (temp < minHeight) {
+                    minColumn = i;
+                    minHeight = temp;
+                };
+            };
+            return minColumn;
+        }
+
         window.onload = function () { show(); };
         window.onresize = function () { show(); };
+        setTimeout(show(), 500);
 
         document.querySelectorAll('.albums').forEach(x => {
             x.onclick = function () {
                 const images = [];
                 var id = this.attributes["data-id"].value;
-                axios.get(`${api_domain}/gallery/images?id=${id}`)
+                axios.get(`https://api.meowv.com/gallery/images?id=${id}`)
                     .then(function (response) {
                         if (response.data.success) {
                             response.data.result.forEach(x => {
@@ -65,39 +97,3 @@ axios.get(`${api_domain}/gallery`)
     }).catch(function (error) {
         console.log(error);
     });
-
-function show() {
-    var columnHeight = [];
-    var bodyWidth = document.body.offsetWidth;
-    var n = parseInt(bodyWidth / boxWidth);
-    var columnNum = n == 0 ? 1 : n;
-    var bodyLeft = bodyWidth >= boxWidth ? bodyWidth - columnNum * boxWidth : 0;
-    con[0].style.left = parseInt(bodyLeft / 2) - margin / 2 + 'px';
-    for (var i = 0, l = boxes.length; i < l; i++) {
-        if (i < columnNum) {
-            columnHeight[i] = boxes[i].offsetHeight + margin;
-            boxes[i].style.top = 0;
-            boxes[i].style.left = i * boxWidth + margin + 'px';
-        } else {
-            var innsertColumn = min(columnHeight),
-                imgHeight = boxes[i].offsetHeight + margin;
-            boxes[i].style.top = columnHeight[innsertColumn] + 'px';
-            boxes[i].style.left = innsertColumn * boxWidth + margin + 'px';
-            columnHeight[innsertColumn] += imgHeight;
-        };
-
-    };
-};
-
-function min(heightAarry) {
-    var minColumn = 0;
-    var minHeight = heightAarry[minColumn];
-    for (var i = 1, len = heightAarry.length; i < len; i++) {
-        var temp = heightAarry[i];
-        if (temp < minHeight) {
-            minColumn = i;
-            minHeight = temp;
-        };
-    };
-    return minColumn;
-}
