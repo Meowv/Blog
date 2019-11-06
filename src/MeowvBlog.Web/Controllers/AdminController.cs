a0bd072498d0d82298d2d5016e3ea028dc415b77
阿星Plus <hackxing@foxmail.com>
2019-11-06 17:21:48 +0800

    开始 博客内容后台管理

---
+++ src/MeowvBlog.Web/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeowvBlog.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        /// <summary>
+        /// 鉴权
+        /// </summary>
+        /// <returns></returns>
+        [Route("/account/auth")]
+        public IActionResult Auth() => View();
+
+        /// <summary>
+        /// 后台管理
+        /// </summary>
+        /// <returns></returns>
+        [Route("/admin")]
+        public IActionResult Index() => View();
+    }
+}
