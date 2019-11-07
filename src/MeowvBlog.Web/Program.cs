934396da7e6965042330ab7127eacd89acb96b25
阿星Plus <hackxing@foxmail.com>
2019-11-07 16:07:04 +0800

    Startup合并至Program

--- src/MeowvBlog.Web/Program.cs
+++ src/MeowvBlog.Web/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
@@ -9,12 +11,26 @@ namespace MeowvBlog.Web
         public static async Task Main(string[] args)
         {
             await Host.CreateDefaultBuilder(args)
-                      .ConfigureWebHostDefaults(builder =>
-                      {
-                          builder.ConfigureKestrel(options => { options.AddServerHeader = false; })
-                                 .UseUrls("http://*:5001")
-                                 .UseStartup<Startup>();
-                      }).Build().RunAsync();
+                        .ConfigureWebHostDefaults(builder =>
+                        {
+                            builder.ConfigureKestrel(options => { options.AddServerHeader = false; })
+                                   .UseUrls("http://*:5001")
+                                   .UseStartup<Program>();
+                        }).Build().RunAsync();
+        }
+
+        public void ConfigureServices(IServiceCollection services) => services.AddControllersWithViews();
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStatusCodePages();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
