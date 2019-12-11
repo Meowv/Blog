095c175c249b81efaa6b163b6868a871011d1644
阿星Plus <hackxing@foxmail.com>
2019-12-11 17:23:43 +0800

    AppSettings配置文件代码注释

--- src/MeowvBlog.API/Configurations/AppSettings.cs
+++ src/MeowvBlog.API/Configurations/AppSettings.cs
@@ -5,12 +5,22 @@ using System.IO;
 
 namespace MeowvBlog.API.Configurations
 {
+    /// <summary>
+    /// appsettings.json配置文件数据读取类
+    /// </summary>
     public class AppSettings
     {
+        /// <summary>
+        /// 配置文件的根节点
+        /// </summary>
         private static readonly IConfigurationRoot _config;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         static AppSettings()
         {
+            // 加载appsettings.json，并构建IConfigurationRoot
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
                                                     .AddJsonFile("Resources/appsettings.json", true, true);
             _config = builder.Build();
