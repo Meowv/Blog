42fc115bc08c342ef13f487f1810765203191f93
阿星Plus <hackxing@foxmail.com>
2019-12-12 15:41:01 +0800

    扩展方法：将转换为byte[]类型的图片保存至指定路径

--- src/MeowvBlog.API/Extensions/Extensions.cs
+++ src/MeowvBlog.API/Extensions/Extensions.cs
@@ -228,5 +228,25 @@ namespace MeowvBlog.API.Extensions
         {
             return source.Select(selector).ToListAsync();
         }
+
+        /// <summary>
+        /// 将转换为byte[]类型的图片保存至指定路径
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static async Task SaveImg(this byte[] buffer, string path)
+        {
+            using var ms = new MemoryStream(buffer);
+            using var stream = new FileStream(path, FileMode.Create);
+
+            var bytes = new byte[1024];
+            var size = await ms.ReadAsync(bytes, 0, bytes.Length);
+            while (size > 0)
+            {
+                await stream.WriteAsync(bytes, 0, size);
+                size = await ms.ReadAsync(bytes, 0, bytes.Length);
+            }
+        }
     }
 }
