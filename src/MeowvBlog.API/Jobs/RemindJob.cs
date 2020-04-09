7e67999d67dc336a65d5199421723f2896c68770
阿星Plus <hackxing@foxmail.com>
2020-04-09 10:25:02 +0800

    PuppeteerSharp Package Test

--- src/MeowvBlog.API/Jobs/RemindJob.cs
+++ src/MeowvBlog.API/Jobs/RemindJob.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Hosting;
+using PuppeteerSharp;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,9 +12,24 @@ namespace MeowvBlog.API.Jobs
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                Console.WriteLine(DateTime.Now);
+                // 初始化浏览器
+                await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
 
-                await Task.Delay(1000, stoppingToken);
+                // 访问指定页面保存为图片
+                using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true }))
+                {
+                    using var page = await browser.NewPageAsync();
+                    await page.GoToAsync("https://github.com/Meowv");
+                    await page.ScreenshotAsync(@"D:\meowv.png", new ScreenshotOptions
+                    {
+                        FullPage = true,
+                        Type = ScreenshotType.Png
+                    });
+                }
+
+                Console.WriteLine("qix:" + DateTime.Now);
+
+                await Task.Delay(10000, stoppingToken);
             }
         }
     }
