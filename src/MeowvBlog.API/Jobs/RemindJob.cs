b9c2d7d79d9c189fe8da9e0ba2e850c5c29794ee
阿星Plus <hackxing@foxmail.com>
2020-04-09 15:02:23 +0800

    基于MailKit和MimeKit发送邮件

--- src/MeowvBlog.API/Jobs/RemindJob.cs
+++ src/MeowvBlog.API/Jobs/RemindJob.cs
@@ -1,8 +1,12 @@
-﻿using MeowvBlog.API.Configurations;
+﻿using MailKit.Net.Smtp;
+using MeowvBlog.API.Configurations;
 using Microsoft.Extensions.Hosting;
+using MimeKit;
+using MimeKit.Utils;
 using PuppeteerSharp;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -38,7 +42,35 @@ namespace MeowvBlog.API.Jobs
 
                     #endregion
 
-                    Console.WriteLine(path);
+                    #region 发送Email
+
+                    var message = new MimeMessage();
+
+                    message.From.Add(new MailboxAddress(AppSettings.Email.From.Name, AppSettings.Email.From.Address));
+                    var address = AppSettings.Email.To.Select(x => new MailboxAddress(x.Key, x.Value));
+                    message.To.AddRange(address);
+                    message.Subject = "【定时任务】Github个人主页截图推送";
+
+                    var builder = new BodyBuilder();
+
+                    var image = builder.LinkedResources.Add(path);
+                    image.ContentId = MimeUtils.GenerateMessageId();
+
+                    builder.HtmlBody = string.Format("<img src=\"cid:{0}\"/>", image.ContentId);
+                    message.Body = builder.ToMessageBody();
+
+                    using (var client = new SmtpClient())
+                    {
+                        client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                        client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                        await client.ConnectAsync(AppSettings.Email.Host, AppSettings.Email.Port, AppSettings.Email.UseSsl);
+                        await client.AuthenticateAsync(AppSettings.Email.From.Username, AppSettings.Email.From.Password);
+                        await client.SendAsync(message);
+                        await client.DisconnectAsync(true);
+                    }
+
+                    #endregion
 
                     // 延迟执行
                     await Task.Delay(AppSettings.Job.MillisecondsDelay, stoppingToken);
