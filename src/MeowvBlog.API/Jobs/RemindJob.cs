836e39a77593999e0acbf1937d5852075e2a1c02
阿星Plus <hackxing@foxmail.com>
2020-04-09 11:33:50 +0800

    访问指定URL,保存为图片

--- src/MeowvBlog.API/Jobs/RemindJob.cs
+++ src/MeowvBlog.API/Jobs/RemindJob.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using MeowvBlog.API.Configurations;
+using Microsoft.Extensions.Hosting;
 using PuppeteerSharp;
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,24 +14,35 @@ namespace MeowvBlog.API.Jobs
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                // 初始化浏览器
-                await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-
-                // 访问指定页面保存为图片
-                using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true }))
+                if (DateTime.Now.Hour >= AppSettings.Job.ExecutionTime)
                 {
+                    #region 访问指定URL,保存为图片
+
+                    var path = Path.Combine(Path.GetTempPath(), "meowv.png");
+
+                    await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+                    using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
                     using var page = await browser.NewPageAsync();
+
+                    await page.SetViewportAsync(new ViewPortOptions
+                    {
+                        Width = 1920,
+                        Height = 1080
+                    });
                     await page.GoToAsync("https://github.com/Meowv");
-                    await page.ScreenshotAsync(@"D:\meowv.png", new ScreenshotOptions
+                    await page.ScreenshotAsync(path, new ScreenshotOptions
                     {
                         FullPage = true,
                         Type = ScreenshotType.Png
                     });
+
+                    #endregion
+
+                    Console.WriteLine(path);
+
+                    // 延迟执行
+                    await Task.Delay(AppSettings.Job.MillisecondsDelay, stoppingToken);
                 }
-
-                Console.WriteLine("qix:" + DateTime.Now);
-
-                await Task.Delay(10000, stoppingToken);
             }
         }
     }
