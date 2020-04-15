dc19c2ff9700a235fa4a1874f81d174e57c4f97d
阿星Plus <hackxing@foxmail.com>
2020-04-15 17:56:06 +0800

    Domain Modularity

---
+++ src/MeowvBlog.Domain.Shared/MeowvBlogDomainSharedModule.cs
@@ -0,0 +1,20 @@
+﻿using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+using Volo.Abp.VirtualFileSystem;
+
+namespace MeowvBlog.Domain.Shared
+{
+    [DependsOn(
+        typeof(AbpIdentityDomainSharedModule)
+    )]
+    public class MeowvBlogDomainSharedModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<MeowvBlogDomainSharedModule>("Meowv");
+            });
+        }
+    }
+}
---
+++ src/MeowvBlog.Domain/MeowvBlogDomainModule.cs
@@ -0,0 +1,18 @@
+﻿using MeowvBlog.Domain.Shared;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+
+namespace MeowvBlog.Domain
+{
+    [DependsOn(
+        typeof(MeowvBlogDomainSharedModule),
+        typeof(AbpIdentityDomainModule)
+    )]
+    public class MeowvBlogDomainModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            base.ConfigureServices(context);
+        }
+    }
+}
