f51f00c2608e9e9511b66ec6b4598d0acee653d4
阿星Plus <hackxing@foxmail.com>
2020-04-24 15:54:46 +0800

    分页参数添加Range限制

--- src/Meowv.Blog.Application.Contracts/PagingInput.cs
+++ src/Meowv.Blog.Application.Contracts/PagingInput.cs
@@ -1,4 +1,6 @@
-﻿namespace Meowv.Blog.Application.Contracts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meowv.Blog.Application.Contracts
 {
     /// <summary>
     /// 分页输入参数
@@ -8,11 +10,13 @@
         /// <summary>
         /// 页码
         /// </summary>
+        [Range(1, int.MaxValue)]
         public int Page { get; set; } = 1;
 
         /// <summary>
         /// 限制条数
         /// </summary>
+        [Range(10, 30)]
         public int Limit { get; set; } = 10;
     }
 }
