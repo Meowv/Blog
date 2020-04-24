2224445b1da1ff5cd9cfd30531b4edd03a02660f
阿星Plus <hackxing@foxmail.com>
2020-04-24 15:06:32 +0800

    时间格式转换

---
+++ src/Meowv.Blog.ToolKits/Extensions/DateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Meowv.Blog.ToolKits.Extensions
+{
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// 时间格式转换
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="format"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public static string ToDateTime(this DateTime date, string format = "MMMM dd, yyyy HH:mm:ss", string cultureInfo = "en-us")
+        {
+            return date.ToString(format, new CultureInfo(cultureInfo));
+        }
+    }
+}
