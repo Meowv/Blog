59387957846bb6365221e53af1c0a44cfae2bcd2
阿星Plus <hackxing@foxmail.com>
2020-04-27 10:25:12 +0800

    signature code first create db table

--- src/Meowv.Blog.EntityFrameworkCore.DbMigrations/Migrations/20200427021900_Signature.Designer.cs
+++ src/Meowv.Blog.EntityFrameworkCore.DbMigrations/Migrations/20200427021900_Signature.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Meowv.Blog.EntityFrameworkCore.DbMigrations.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Meowv.Blog.EntityFrameworkCore.DbMigrations.Migrations
 {
     [DbContext(typeof(MeowvBlogMigrationsDbContext))]
-    partial class MeowvBlogMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200427021900_Signature")]
+    partial class Signature
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
