913291d8e3fe2be26fdf0f09f7fe7ce585f578fa
阿星Plus <hackxing@foxmail.com>
2020-04-28 17:08:08 +0800

    移除Swagger，改为从Module引用

--- src/Meowv.Blog.HttpApi.Hosting/MeowvBlogHttpApiHostingModule.cs
+++ src/Meowv.Blog.HttpApi.Hosting/MeowvBlogHttpApiHostingModule.cs
@@ -1,11 +1,8 @@
-using Hangfire;
-using Hangfire.Dashboard.BasicAuthorization;
 using Meowv.Blog.BackgroundJobs;
-using Meowv.Blog.Domain;
 using Meowv.Blog.Domain.Configurations;
 using Meowv.Blog.EntityFrameworkCore;
 using Meowv.Blog.HttpApi.Hosting.Middleware;
-using Meowv.Blog.HttpApi.Hosting.Swagger;
+using Meowv.Blog.Swagger;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -21,12 +18,12 @@ using Volo.Abp.Modularity;
 namespace Meowv.Blog.HttpApi.Hosting
 {
     [DependsOn(
-        typeof(MeowvBlogHttpApiModule),
-        typeof(MeowvBlogDomainModule),
-        typeof(MeowvBlogFrameworkCoreModule),
         typeof(AbpAspNetCoreMvcModule),
-        typeof(MeowvBlogBackgroundJobsModule),
-        typeof(AbpAutofacModule)
+        typeof(AbpAutofacModule),
+        typeof(MeowvBlogHttpApiModule),
+        typeof(MeowvBlogSwaggerModule),
+        typeof(MeowvBlogFrameworkCoreModule),
+        typeof(MeowvBlogBackgroundJobsModule)
         )]
     public class MeowvBlogHttpApiHostingModule : AbpModule
     {
@@ -41,35 +38,34 @@ namespace Meowv.Blog.HttpApi.Hosting
                 options.AppendTrailingSlash = true;
             });
 
-            // Swagger扩展
-            context.Services.AddSwagger();
-
             // 身份验证之JWT
             context.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                    .AddJwtBearer(options =>
-                    {
-                        options.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            // 是否验证颁发者
-                            ValidateIssuer = true,
-                            // 是否验证访问群体
-                            ValidateAudience = true,
-                            // 是否验证生存期
-                            ValidateLifetime = true,
-                            // 验证Token的时间偏移量
-                            ClockSkew = TimeSpan.FromSeconds(30),
-                            // 是否验证安全密钥
-                            ValidateIssuerSigningKey = true,
-                            // 访问群体
-                            ValidAudience = AppSettings.JWT.Domain,
-                            // 颁发者
-                            ValidIssuer = AppSettings.JWT.Domain,
-                            // 安全密钥
-                            IssuerSigningKey = new SymmetricSecurityKey(AppSettings.JWT.SecurityKey.GetBytes())
-                        };
-                    });
+                   .AddJwtBearer(options =>
+                   {
+                       options.TokenValidationParameters = new TokenValidationParameters
+                       {
+                           // 是否验证颁发者
+                           ValidateIssuer = true,
+                           // 是否验证访问群体
+                           ValidateAudience = true,
+                           // 是否验证生存期
+                           ValidateLifetime = true,
+                           // 验证Token的时间偏移量
+                           ClockSkew = TimeSpan.FromSeconds(30),
+                           // 是否验证安全密钥
+                           ValidateIssuerSigningKey = true,
+                           // 访问群体
+                           ValidAudience = AppSettings.JWT.Domain,
+                           // 颁发者
+                           ValidIssuer = AppSettings.JWT.Domain,
+                           // 安全密钥
+                           IssuerSigningKey = new SymmetricSecurityKey(AppSettings.JWT.SecurityKey.GetBytes())
+                       };
+                   });
+
             // 认证授权
             context.Services.AddAuthorization();
+
             // Http请求
             context.Services.AddHttpClient();
         }
@@ -112,12 +108,6 @@ namespace Meowv.Blog.HttpApi.Hosting
 
             // HTTP => HTTPS
             app.UseHttpsRedirection();
-
-            // Swagger
-            app.UseSwagger();
-
-            // SwaggerUI
-            app.UseSwaggerUI();
 
             // 路由映射
             app.UseEndpoints(endpoints =>
