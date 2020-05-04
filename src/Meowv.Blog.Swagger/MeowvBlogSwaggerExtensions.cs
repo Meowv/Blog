081232fa081dfa1847c99ce14b8d15701a96be19
阿星Plus <hackxing@foxmail.com>
2020-05-04 22:35:26 +0800

    Swagger描述信息

--- src/Meowv.Blog.Swagger/MeowvBlogSwaggerExtensions.cs
+++ src/Meowv.Blog.Swagger/MeowvBlogSwaggerExtensions.cs
@@ -21,7 +21,7 @@ namespace Meowv.Blog.Swagger
         /// <summary>
         /// Swagger描述信息
         /// </summary>
-        private static readonly string description = @"<b>Blog</b>：<a target=""_blank"" href=""https://meowv.com"">https://meowv.com</a> <b>GitHub</b>：<a target=""_blank"" href=""https://github.com/Meowv/Blog"">https://github.com/Meowv/Blog</a> <code>Powered by .NET Core 3.1 on Linux</code>";
+        private static readonly string description = @"<b>Blog</b>：<a target=""_blank"" href=""https://meowv.com"">https://meowv.com</a> <b>GitHub</b>：<a target=""_blank"" href=""https://github.com/Meowv/Blog"">https://github.com/Meowv/Blog</a> <b>Hangfire</b>：<a target=""_blank"" href=""/hangfire"">任务调度中心</a> <code>Powered by .NET Core 3.1 on Linux</code>";
 
         /// <summary>
         /// Swagger分组信息，将进行遍历使用
