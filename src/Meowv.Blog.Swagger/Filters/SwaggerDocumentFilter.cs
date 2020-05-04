8522672eca8a41d6caab59a4604bb133a86d6267
阿星Plus <hackxing@foxmail.com>
2020-05-04 22:30:23 +0800

    当前的controller信息

--- src/Meowv.Blog.Swagger/Filters/SwaggerDocumentFilter.cs
+++ src/Meowv.Blog.Swagger/Filters/SwaggerDocumentFilter.cs
@@ -25,45 +25,51 @@ namespace Meowv.Blog.Swagger.Filters
                     ExternalDocs = new OpenApiExternalDocs { Description = "文章/标签/分类/友链/RSS" }
                 },
                 new OpenApiTag {
-                    Name = "Common",
-                    Description = "通用公共接口",
-                    ExternalDocs = new OpenApiExternalDocs { Description = "每日热点/Bing壁纸/妹子图/随机一张猫图/语音合成/智能抠图" }
-                },
-                new OpenApiTag {
-                    Name = "MTA",
-                    Description = "腾讯移动分析",
-                    ExternalDocs = new OpenApiExternalDocs { Description = "MTA" }
-                },
-                new OpenApiTag {
-                    Name = "TCA",
-                    Description = "腾讯云API",
-                    ExternalDocs = new OpenApiExternalDocs { Description = "CDN刷新/验证码校验" }
-                },
-                new OpenApiTag {
                     Name = "Signature",
                     Description = "个性艺术签名设计",
                     ExternalDocs = new OpenApiExternalDocs { Description = "Signature" }
                 },
                 new OpenApiTag {
-                    Name = "Gallery",
-                    Description = "图集相册",
-                    ExternalDocs = new OpenApiExternalDocs { Description = "Gallery" }
-                },
-                new OpenApiTag {
-                    Name = "Soul",
-                    Description = "心灵/毒 鸡汤",
-                    ExternalDocs = new OpenApiExternalDocs { Description = "Soul" }
-                },
-                new OpenApiTag {
-                    Name = "FM",
-                    Description = "FM电台",
-                    ExternalDocs = new OpenApiExternalDocs { Description = "FM" }
-                },
-                new OpenApiTag {
                     Name = "Wallpaper",
                     Description = "壁纸",
                     ExternalDocs = new OpenApiExternalDocs { Description = "Wallpaper" }
-                }
+                },
+                new OpenApiTag
+                {
+                    Name = "HotNews",
+                    Description = "每日热点接口",
+                    ExternalDocs = new OpenApiExternalDocs { Description = "每日热点来源和列表" }
+                },
+                //new OpenApiTag {
+                //    Name = "Common",
+                //    Description = "通用公共接口",
+                //    ExternalDocs = new OpenApiExternalDocs { Description = "每日热点/Bing壁纸/妹子图/随机一张猫图/语音合成/智能抠图" }
+                //},
+                //new OpenApiTag {
+                //    Name = "MTA",
+                //    Description = "腾讯移动分析",
+                //    ExternalDocs = new OpenApiExternalDocs { Description = "MTA" }
+                //},
+                //new OpenApiTag {
+                //    Name = "TCA",
+                //    Description = "腾讯云API",
+                //    ExternalDocs = new OpenApiExternalDocs { Description = "CDN刷新/验证码校验" }
+                //},
+                //new OpenApiTag {
+                //    Name = "Gallery",
+                //    Description = "图集相册",
+                //    ExternalDocs = new OpenApiExternalDocs { Description = "Gallery" }
+                //},
+                //new OpenApiTag {
+                //    Name = "Soul",
+                //    Description = "心灵/毒 鸡汤",
+                //    ExternalDocs = new OpenApiExternalDocs { Description = "Soul" }
+                //},
+                //new OpenApiTag {
+                //    Name = "FM",
+                //    Description = "FM电台",
+                //    ExternalDocs = new OpenApiExternalDocs { Description = "FM" }
+                //}
             };
 
             // 按照Name升序排序
