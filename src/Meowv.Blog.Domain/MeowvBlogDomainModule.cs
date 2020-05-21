d895de0c4660ef7036bc186db9f8b5ff776777b9
阿星Plus <hackxing@foxmail.com>
2020-05-21 13:30:14 +0800

    解决项目相互之间引用不合理的bug

--- src/Meowv.Blog.Domain/MeowvBlogDomainModule.cs
+++ src/Meowv.Blog.Domain/MeowvBlogDomainModule.cs
@@ -1,9 +1,13 @@
+using Meowv.Blog.Domain.Shared;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 
 namespace Meowv.Blog.Domain
 {
-    [DependsOn(typeof(AbpIdentityDomainModule))]
+    [DependsOn(
+        typeof(AbpIdentityDomainModule),
+        typeof(MeowvBlogDomainSharedModule)
+    )]
     public class MeowvBlogDomainModule : AbpModule
     {
 
