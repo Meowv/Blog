a9aafb687a23e790a9db5abb7c97d9d6b34eaf67
阿星Plus <hackxing@foxmail.com>
2020-05-23 13:55:00 +0800

    实现添加自定义描述时过滤不属于同一个分组的API

--- src/Meowv.Blog.Swagger/Filters/SwaggerDocumentFilter.cs
+++ src/Meowv.Blog.Swagger/Filters/SwaggerDocumentFilter.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Meowv.Blog.Swagger.Filters
 {
@@ -31,8 +34,22 @@ namespace Meowv.Blog.Swagger.Filters
                 }
             };
 
-            // 按照Name升序排序
-            swaggerDoc.Tags = tags.OrderBy(x => x.Name).ToList();
+            #region 实现添加自定义描述时过滤不属于同一个分组的API
+
+            // 当前分组名称
+            var groupName = context.ApiDescriptions.FirstOrDefault().GroupName;
+
+            // 当前所有的API对象
+            var apis = context.ApiDescriptions.GetType().GetField("_source", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(context.ApiDescriptions) as IEnumerable<ApiDescription>;
+
+            // 不属于当前分组的所有Controller
+            // 注意：配置的OpenApiTag，Name名称要与Controller的Name对应才会生效。
+            var controllers = apis.Where(x => x.GroupName != groupName).Select(x => ((ControllerActionDescriptor)x.ActionDescriptor).ControllerName).Distinct();
+
+            // 筛选一下tags
+            swaggerDoc.Tags = tags.Where(x => !controllers.Contains(x.Name)).OrderBy(x => x.Name).ToList();
+
+            #endregion
         }
     }
 }
