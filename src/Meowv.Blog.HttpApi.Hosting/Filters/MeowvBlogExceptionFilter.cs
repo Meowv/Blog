e5a7e00c71897b89bfc6ca6f15e430a56bd75837
阿星Plus <hackxing@foxmail.com>
2020-05-25 17:30:10 +0800

    优化 log4net 日志记录调用方式

--- src/Meowv.Blog.HttpApi.Hosting/Filters/MeowvBlogExceptionFilter.cs
+++ src/Meowv.Blog.HttpApi.Hosting/Filters/MeowvBlogExceptionFilter.cs
@@ -1,10 +1,17 @@
-﻿using Meowv.Blog.ToolKits.Helper;
+﻿using log4net;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Meowv.Blog.HttpApi.Hosting.Filters
 {
     public class MeowvBlogExceptionFilter : IExceptionFilter
     {
+        private readonly ILog _log;
+
+        public MeowvBlogExceptionFilter()
+        {
+            _log = LogManager.GetLogger(typeof(MeowvBlogExceptionFilter));
+        }
+
         /// <summary>
         /// 异常处理
         /// </summary>
@@ -12,8 +19,8 @@ namespace Meowv.Blog.HttpApi.Hosting.Filters
         /// <returns></returns>
         public void OnException(ExceptionContext context)
         {
-            // 日志记录
-            LoggerHelper.WriteToFile($"{context.HttpContext.Request.Path}|{context.Exception.Message}", context.Exception);
+            // 错误日志记录
+            _log.Error($"{context.HttpContext.Request.Path}|{context.Exception.Message}", context.Exception);
         }
     }
 }
--- src/Meowv.Blog.HttpApi.Hosting/Program.cs
+++ src/Meowv.Blog.HttpApi.Hosting/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Meowv.Blog.ToolKits.Extensions;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Meowv.Blog.HttpApi.Hosting
         public static async Task Main(string[] args)
         {
             await Host.CreateDefaultBuilder(args)
+                      .UseLog4Net()
                       .ConfigureWebHostDefaults(builder =>
                       {
                           builder.UseIISIntegration()
