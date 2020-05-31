33acf33da9d1e968764a751d4019375864b3804f
阿星Plus <hackxing@foxmail.com>
2020-05-31 20:39:53 +0800

    查询标签列表

--- src/Meowv.Blog.Application.Caching/Blog/Impl/BlogCacheService.Tag.cs
+++ src/Meowv.Blog.Application.Caching/Blog/Impl/BlogCacheService.Tag.cs
@@ -1,7 +1,24 @@
-﻿namespace Meowv.Blog.Application.Caching.Blog.Impl
+﻿using Meowv.Blog.Application.Contracts.Blog;
+using Meowv.Blog.ToolKits.Base;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static Meowv.Blog.Domain.Shared.MeowvBlogConsts;
+
+namespace Meowv.Blog.Application.Caching.Blog.Impl
 {
     public partial class BlogCacheService
     {
-        
+        private const string KEY_QueryTags = "Blog:Tag:QueryTags";
+
+        /// <summary>
+        /// 查询标签列表
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <returns></returns>
+        public async Task<ServiceResult<IEnumerable<QueryTagDto>>> QueryTagsAsync(Func<Task<ServiceResult<IEnumerable<QueryTagDto>>>> factory)
+        {
+            return await Cache.GetOrAddAsync(KEY_QueryTags, factory, CacheStrategy.ONE_DAY);
+        }
     }
 }
