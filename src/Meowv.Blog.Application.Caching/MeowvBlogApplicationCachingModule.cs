f693480b7f6acb6bfdd755e5a515bddfaec693f4
阿星Plus <hackxing@foxmail.com>
2020-06-03 22:46:11 +0800

    Caching.CSRedis test

--- src/Meowv.Blog.Application.Caching/MeowvBlogApplicationCachingModule.cs
+++ src/Meowv.Blog.Application.Caching/MeowvBlogApplicationCachingModule.cs
@@ -1,5 +1,7 @@
 ﻿using Meowv.Blog.Domain;
 using Meowv.Blog.Domain.Configurations;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Redis;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Caching;
 using Volo.Abp.Modularity;
@@ -18,6 +20,11 @@ namespace Meowv.Blog.Application.Caching
             {
                 options.Configuration = AppSettings.Caching.RedisConnectionString;
             });
+
+            var csredis = new CSRedis.CSRedisClient(AppSettings.Caching.RedisConnectionString);
+            RedisHelper.Initialization(csredis);
+
+            context.Services.AddSingleton<IDistributedCache>(new CSRedisCache(RedisHelper.Instance));
         }
     }
 }
