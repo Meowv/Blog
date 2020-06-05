b2f0ed591d09922d48b715bcce03aa454fb5f83a
阿星Plus <hackxing@foxmail.com>
2020-06-05 17:28:53 +0800

    Meowv.Blog.BlazorApp Create

---
+++ src/Meowv.Blog.BlazorApp/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.BlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
