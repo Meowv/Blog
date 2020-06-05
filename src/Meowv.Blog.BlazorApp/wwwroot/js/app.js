7177f1bb505cb49143579f996247c1451d8e0a5e
阿星Plus <hackxing@foxmail.com>
2020-06-06 00:01:08 +0800

    Blazor App Study

---
+++ src/Meowv.Blog.BlazorApp/wwwroot/js/app.js
@@ -0,0 +1,18 @@
+﻿window.func = {
+    setStorage: function (name, value) {
+        localStorage.setItem(name, value)
+    },
+    getStorage: function (name) {
+        return localStorage.getItem(name);
+    },
+    switchTheme: function () {
+        var currentTheme = this.getStorage('theme') || 'white';
+        var isDark = currentTheme === 'dark';
+
+        if (isDark) {
+            document.querySelector('body').classList.add('dark-theme');
+        } else {
+            document.querySelector('body').classList.remove('dark-theme');
+        }
+    }
+}
