9c8bbb17a1057d1b3201f15b14c6537b42b169b6
阿星Plus <hackxing@foxmail.com>
2020-06-11 09:48:45 +0800

    标签管理列表

---
+++ src/Meowv.Blog.BlazorApp/Response/Blog/QueryFriendLinkForAdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Meowv.Blog.BlazorApp.Response.Blog
+{
+    public class QueryFriendLinkForAdminDto : FriendLinkDto
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
