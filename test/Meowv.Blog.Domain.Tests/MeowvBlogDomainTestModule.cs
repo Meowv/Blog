3bf4544ac78dbe13e0fe0fd1a9f467475309c34b
阿星Plus <hackxing@foxmail.com>
2020-07-13 17:43:56 +0800

    Meowv.Blog.Domain.Tests

--- test/Meowv.Blog.Domain.Tests/MeowvBlogDomainTestModule.cs
+++ test/Meowv.Blog.Domain.Tests/MeowvBlogDomainTestModule.cs
@@ -1,7 +1,9 @@
-﻿using Volo.Abp.Modularity;
+﻿using Meowv.Blog.EntityFrameworkCore.Tests.EntityFrameworkCore;
+using Volo.Abp.Modularity;
 
 namespace Meowv.Blog.Domain.Tests
 {
+    [DependsOn(typeof(MeowvBlogEntityFrameworkCoreTestModule))]
     public class MeowvBlogDomainTestModule : AbpModule
     {
     }
