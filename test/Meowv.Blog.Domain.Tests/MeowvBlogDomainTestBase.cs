0e973938bc509a799e74945f3752b1fb50f0177e
阿星Plus <hackxing@foxmail.com>
2020-07-13 16:27:37 +0800

    MeowvBlog Domain Test

---
+++ test/Meowv.Blog.Domain.Tests/MeowvBlogDomainTestBase.cs
@@ -0,0 +1,8 @@
+﻿using Meowv.Blog.TestBase;
+
+namespace Meowv.Blog.Domain.Tests
+{
+    public abstract class MeowvBlogDomainTestBase : MeowvBlogTestBase<MeowvBlogDomainTestModule>
+    {
+    }
+}
---
+++ test/Meowv.Blog.Domain.Tests/MeowvBlogDomainTestModule.cs
@@ -0,0 +1,8 @@
+﻿using Volo.Abp.Modularity;
+
+namespace Meowv.Blog.Domain.Tests
+{
+    public class MeowvBlogDomainTestModule : AbpModule
+    {
+    }
+}
