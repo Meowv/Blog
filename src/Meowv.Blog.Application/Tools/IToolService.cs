2d3ff95af1155bf29bc1a3975768db06d64379c0
阿星Plus <hackxing@foxmail.com>
2021-01-18 21:58:02 +0800

    Get bing background

--- src/Meowv.Blog.Application/Tools/IToolService.cs
+++ src/Meowv.Blog.Application/Tools/IToolService.cs
@@ -1,5 +1,6 @@
 ﻿using Meowv.Blog.Dto.Tools.Params;
 using Meowv.Blog.Response;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,6 +8,10 @@ namespace Meowv.Blog.Tools
 {
     public interface IToolService
     {
+        Task<BlogResponse<string>> GetBingBackgroundUrlAsync();
+
+        Task<FileContentResult> GetBingBackgroundImgAsync();
+
         Task<BlogResponse<List<string>>> Ip2RegionAsync(string ip);
 
         Task<BlogResponse> SendMessageAsync(SendMessageInput input);
--- src/Meowv.Blog.Application/Tools/Impl/ToolService.cs
+++ src/Meowv.Blog.Application/Tools/Impl/ToolService.cs
@@ -4,6 +4,7 @@ using Meowv.Blog.Extensions;
 using Meowv.Blog.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,6 +24,42 @@ namespace Meowv.Blog.Tools.Impl
         {
             _httpClient = httpClient;
             _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Get bing background url.
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/meowv/tool/bing/url")]
+        public async Task<BlogResponse<string>> GetBingBackgroundUrlAsync()
+        {
+            var response = new BlogResponse<string>();
+
+            var api = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&pid=hp&FORM=BEHPTB";
+
+            using var client = _httpClient.CreateClient();
+            var json = await client.GetStringAsync(api);
+
+            var obj = JObject.Parse(json);
+            var url = $"https://cn.bing.com{obj["images"].First()["url"]}";
+
+            response.Result = url;
+            return response;
+        }
+
+        /// <summary>
+        /// Get bing background image.
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/meowv/tool/bing/img")]
+        public async Task<FileContentResult> GetBingBackgroundImgAsync()
+        {
+            var url = (await GetBingBackgroundUrlAsync()).Result;
+
+            using var client = _httpClient.CreateClient();
+            var bytes = await client.GetByteArrayAsync(url);
+
+            return new FileContentResult(bytes, "image/jpeg");
         }
 
         /// <summary>
@@ -74,6 +111,7 @@ namespace Meowv.Blog.Tools.Impl
 
             var content = new StringContent($"text={input.Text}&desp={input.Desc}");
             content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
             using var client = _httpClient.CreateClient();
             await client.PostAsync("https://sc.ftqq.com/SCU60393T5a94df1d5a9274125293f34a6acf928f5d78f551cf6d6.send", content);
 
