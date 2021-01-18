588d3fc20904389167452d853660305582c62d3f
阿星Plus <hackxing@foxmail.com>
2021-01-18 20:46:14 +0800

    事件调用Send a message to weixin

--- src/Meowv.Blog.Application/EventHandler/Messages/MessageEventHandler.cs
+++ src/Meowv.Blog.Application/EventHandler/Messages/MessageEventHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Meowv.Blog.Domain.Messages;
+using Meowv.Blog.Dto.Tools.Params;
+using Meowv.Blog.Tools;
 using System.Threading.Tasks;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Entities.Events;
@@ -9,12 +11,20 @@ namespace Meowv.Blog.EventHandler.Messages
     public class MessageEventHandler : ILocalEventHandler<EntityCreatedEventData<Message>>,
                                        ITransientDependency
     {
+        private readonly IToolService _toolService;
+
+        public MessageEventHandler(IToolService toolService)
+        {
+            _toolService = toolService;
+        }
+
         public async Task HandleEventAsync(EntityCreatedEventData<Message> eventData)
         {
-            var title = $"Message from {eventData.Entity.Name}";
-            var content = eventData.Entity.Content;
-
-            await Task.CompletedTask;
+            await _toolService.SendMessageAsync(new SendMessageInput
+            {
+                Text = $"消息来自：{eventData.Entity.Name}",
+                Desc = eventData.Entity.Content
+            });
         }
     }
 }
