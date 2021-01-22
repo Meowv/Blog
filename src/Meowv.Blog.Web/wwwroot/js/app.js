c7790def10b75cda0f56a5b9c44146ed91fe4b82
阿星Plus <hackxing@foxmail.com>
2021-01-22 16:52:46 +0800

    首页搭建，主题切换，二维码显示与隐藏

--- src/Meowv.Blog.Web/wwwroot/js/app.js
+++ src/Meowv.Blog.Web/wwwroot/js/app.js
@@ -1,1 +1,52 @@
-﻿
+﻿window.onload = function () {
+    const currentTheme = window.localStorage.getItem('theme');
+    const isDark = currentTheme === 'dark';
+
+    if (isDark) {
+        document.querySelector('body').classList.add('dark-theme');
+        document.getElementById('switch_default').checked = true;
+        document.getElementById('mobile-toggle-theme').innerText = ' · Dark';
+    } else {
+        document.querySelector('body').classList.remove('dark-theme');
+        document.getElementById('switch_default').checked = false;
+        document.getElementById('mobile-toggle-theme').innerText = ' · Light';
+    }
+
+    document.querySelector('.toggleBtn').addEventListener('click', () => {
+        if (document.querySelector('body').classList.contains('dark-theme')) {
+            document.querySelector('body').classList.remove('dark-theme');
+        } else {
+            document.querySelector('body').classList.add('dark-theme');
+        }
+
+        window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
+    });
+
+    document.getElementById('mobile-toggle-theme').addEventListener('click', () => {
+        if (document.querySelector('body').classList.contains('dark-theme')) {
+            document.querySelector('body').classList.remove('dark-theme');
+            document.getElementById('mobile-toggle-theme').innerText = ' · Light';
+        } else {
+            document.querySelector('body').classList.add('dark-theme');
+            document.getElementById('mobile-toggle-theme').innerText = ' · Dark';
+        }
+
+        window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
+    });
+
+    document.querySelector('.menu-toggle').addEventListener('click', () => {
+        var toggleMenu = document.querySelector('.menu-toggle');
+        var mobileMenu = document.getElementById('mobile-menu');
+        if (toggleMenu.classList.contains('active')) {
+            toggleMenu.classList.remove('active');
+            mobileMenu.classList.remove('active');
+        } else {
+            toggleMenu.classList.add("active");
+            mobileMenu.classList.add("active");
+        }
+    });
+
+    document.querySelector('.weixin').addEventListener('click', () => {
+        document.querySelector(".qrcode").classList.contains('hidden') ? document.querySelector(".qrcode").classList.remove('hidden') : document.querySelector(".qrcode").classList.add('hidden');
+    });
+};
