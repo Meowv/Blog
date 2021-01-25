e6610090c0f3ab0d269cb3a97aceeddaad3d46b5
阿星Plus <hackxing@foxmail.com>
2021-01-25 15:35:48 +0800

    admin ui

--- src/Meowv.Blog.Admin/Program.cs
+++ src/Meowv.Blog.Admin/Program.cs
@@ -1,6 +1,9 @@
+﻿using AntDesign.Pro.Layout;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Meowv.Blog.Admin
@@ -19,6 +22,12 @@ namespace Meowv.Blog.Admin
                                services.AddRazorPages();
                                services.AddServerSideBlazor();
                                services.AddAntDesign();
+                               services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44380") });
+                               services.Configure<ProSettings>(x =>
+                               {
+                                   x.Title = "阿星Plus";
+                                   x.HeaderHeight = 50;
+                               });
                            });
             await host.Build().RunAsync();
         }
