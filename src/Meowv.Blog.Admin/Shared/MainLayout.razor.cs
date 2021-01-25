f2dc579117a1bbc9d75f8e6eff7eace535889520
阿星Plus <hackxing@foxmail.com>
2021-01-25 16:03:08 +0800

    partial class

---
+++ src/Meowv.Blog.Admin/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using AntDesign.Pro.Layout;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.Admin.Shared
+{
+    public partial class MainLayout
+    {
+        private MenuDataItem[] MenuData { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            MenuData = new MenuDataItem[]
+            {
+                new MenuDataItem
+                {
+                    Name = "Dashboard",
+                    Key = "dashboard",
+                    Path = "/",
+                    Icon = "dashboard"
+                },
+            };
+        }
+    }
+}
