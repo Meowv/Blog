3d0f1ed5e839790d754a3450d07b3aac147654c2
阿星Plus <hackxing@foxmail.com>
2021-01-26 12:00:21 +0800

    login page.

--- src/Meowv.Blog.Admin/Models/LoginModel.cs
+++ src/Meowv.Blog.Admin/Models/LoginModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace Meowv.Blog.Admin.Models
 {
-    public class AccountInput
+    public class LoginModel
     {
+        public string LoginType { get; set; }
+
         public string Username { get; set; }
 
         public string Password { get; set; }
+
+        public string Code { get; set; }
     }
 }
---
+++ src/Meowv.Blog.Admin/Pages/OAuth/Login.razor.cs
@@ -0,0 +1,33 @@
+﻿using AntDesign;
+using Meowv.Blog.Admin.Models;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.Admin.Pages.OAuth
+{
+    public partial class Login
+    {
+        private readonly LoginModel input = new LoginModel();
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        [Inject] public MessageService Message { get; set; }
+
+        public void HandleSubmit()
+        {
+            //if (_model.UserName == "admin" && _model.Password == "ant.design")
+            //{
+            //    NavigationManager.NavigateTo("/");
+            //    return;
+            //}
+
+            //if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
+        }
+
+        public async Task GetCode()
+        {
+            //var captcha = await AccountService.GetCaptchaAsync(_model.Mobile);
+            //await Message.Success($"获取验证码成功！验证码为：{captcha}");
+        }
+    }
+}
