3fc1d03c2b326310c0a88b2ca723e0c028fa6d16
阿星Plus <hackxing@foxmail.com>
2021-01-26 11:34:10 +0800

    using npm , gulp build less

---
+++ src/Meowv.Blog.Admin/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+  cleanCss = require('gulp-clean-css'),
+  less = require('gulp-less'),
+  rename = require('gulp-rename'),
+  concatCss = require("gulp-concat-css"),
+  npmImport = require("less-plugin-npm-import");
+
+gulp.task('less', function () {
+  return gulp
+    .src([
+        '**/*.less',
+        '!node_modules/**'
+    ])
+    .pipe(less({
+        javascriptEnabled: true,
+        plugins: [new npmImport({ prefix: '~' })]
+      }))
+    .pipe(concatCss('app.css'))
+    .pipe(cleanCss({ compatibility: '*' }))
+    .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('default', gulp.parallel('less'), function () { })
