95c79094d9b8b4a099f77a055d3742ca187681f9
阿星Plus <hackxing@foxmail.com>
2021-01-28 12:57:46 +0800

    友链列表和删除数据页面绑定

---
+++ src/Meowv.Blog.Admin/Pages/FriendLinks/FriendLinkList.razor.cs
@@ -0,0 +1,38 @@
+﻿using Meowv.Blog.Dto.Blog;
+using Meowv.Blog.Response;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.Admin.Pages.FriendLinks
+{
+    public partial class FriendLinkList
+    {
+        List<GetAdminFriendLinkDto> links;
+
+        protected override async Task OnInitializedAsync()
+        {
+            links = await GetFriendLinkListAsync();
+        }
+
+        public async Task<List<GetAdminFriendLinkDto>> GetFriendLinkListAsync()
+        {
+            var response = await GetResultAsync<BlogResponse<List<GetAdminFriendLinkDto>>>("api/meowv/blog/admin/friendlinks");
+            return response.Result;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var response = await GetResultAsync<BlogResponse>($"api/meowv/blog/friendlink/{id}", method: HttpMethod.Delete);
+            if (response.Success)
+            {
+                await Message.Success("删除成功", 0.5);
+                links = await GetFriendLinkListAsync();
+            }
+            else
+            {
+                await Message.Error("删除失败");
+            }
+        }
+    }
+}
