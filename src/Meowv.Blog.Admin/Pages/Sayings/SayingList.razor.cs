0d42e7da78ae17c609a8b89f9ff723dbe2f612fd
阿星Plus <hackxing@foxmail.com>
2021-01-28 14:37:09 +0800

    毒鸡汤列表绑定

---
+++ src/Meowv.Blog.Admin/Pages/Sayings/SayingList.razor.cs
@@ -0,0 +1,50 @@
+﻿using AntDesign;
+using Meowv.Blog.Dto.Sayings;
+using Meowv.Blog.Response;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.Admin.Pages.Sayings
+{
+    public partial class SayingList
+    {
+        int page = 1;
+        int limit = 10;
+        int total = 0;
+        IReadOnlyList<SayingDto> sayings;
+
+        protected override async Task OnInitializedAsync()
+        {
+            sayings = await GetSayingListAsync(page, limit);
+        }
+
+        public async Task HandlePageIndexChange(PaginationEventArgs args)
+        {
+            sayings = await GetSayingListAsync(page, limit);
+        }
+
+        public async Task<IReadOnlyList<SayingDto>> GetSayingListAsync(int page, int limit)
+        {
+            var response = await GetResultAsync<BlogResponse<PagedList<SayingDto>>>($"api/meowv/sayings/{page}/{limit}");
+
+            total = response.Result.Total;
+
+            return response.Result.Item;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var response = await GetResultAsync<BlogResponse>($"api/meowv/saying/{id}", method: HttpMethod.Delete);
+            if (response.Success)
+            {
+                await Message.Success("删除成功", 0.5);
+                sayings = await GetSayingListAsync(page, limit);
+            }
+            else
+            {
+                await Message.Error("删除失败");
+            }
+        }
+    }
+}
