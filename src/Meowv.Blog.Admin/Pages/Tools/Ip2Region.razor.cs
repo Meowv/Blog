3e7ae69e7dfdea83f646acb9e87ba5a62d6b8670
阿星Plus <hackxing@foxmail.com>
2021-01-29 13:06:33 +0800

    IP查询

---
+++ src/Meowv.Blog.Admin/Pages/Tools/Ip2Region.razor.cs
@@ -0,0 +1,34 @@
+﻿using Meowv.Blog.Response;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.Admin.Pages.Tools
+{
+    public partial class Ip2Region
+    {
+        string ip;
+        string returnIp;
+        string region;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await OnSearch();
+        }
+
+        public async Task OnSearch()
+        {
+            var response = await GetResultAsync<BlogResponse<List<string>>>($"api/meowv/tool/ip2region?ip={ip}");
+            if (!response.Success)
+            {
+                await Message.Error(response.Message);
+            }
+            else
+            {
+                var result = response.Result;
+                returnIp = result.First();
+                region = string.Join(" ", result.Skip(1));
+            }
+        }
+    }
+}
