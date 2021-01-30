dea063a789b57255a71d93319108d1a1561cda7c
阿星Plus <hackxing@foxmail.com>
2021-01-30 14:38:44 +0800

    新增和编辑友链

--- src/Meowv.Blog.Admin/Pages/FriendLinks/FriendLinkList.razor.cs
+++ src/Meowv.Blog.Admin/Pages/FriendLinks/FriendLinkList.razor.cs
@@ -1,5 +1,8 @@
 ﻿using Meowv.Blog.Dto.Blog;
+using Meowv.Blog.Dto.Blog.Params;
 using Meowv.Blog.Response;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,6 +12,14 @@ namespace Meowv.Blog.Admin.Pages.FriendLinks
     public partial class FriendLinkList
     {
         List<GetAdminFriendLinkDto> links;
+
+        bool visible = false;
+
+        string friendlinkId;
+
+        UpdateFriendLinkInput input = new UpdateFriendLinkInput();
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
@@ -32,6 +43,39 @@ namespace Meowv.Blog.Admin.Pages.FriendLinks
             else
             {
                 await Message.Error("删除失败");
+            }
+        }
+
+        private void Close() => visible = false;
+
+        private void Open(GetAdminFriendLinkDto dto)
+        {
+            friendlinkId = dto.Id;
+            input.Name = dto.Name;
+            input.Url = dto.Url;
+
+            visible = true;
+        }
+
+        public async Task HandleSubmit()
+        {
+            if (string.IsNullOrWhiteSpace(input.Name) || string.IsNullOrWhiteSpace(input.Url))
+            {
+                return;
+            }
+
+            var json = JsonConvert.SerializeObject(input);
+
+            var response = await GetResultAsync<BlogResponse>($"api/meowv/blog/friendlink/{friendlinkId}", json, HttpMethod.Put);
+            if (response.Success)
+            {
+                Close();
+                await Message.Success("Successful", 0.5);
+                links = await GetFriendLinkListAsync();
+            }
+            else
+            {
+                await Message.Error(response.Message);
             }
         }
     }
