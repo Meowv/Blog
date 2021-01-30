dcf7c0b2afafdded8f13ec75887d5f9dbfb3e74b
阿星Plus <hackxing@foxmail.com>
2021-01-30 13:33:49 +0800

    新增分类

---
+++ src/Meowv.Blog.Admin/Pages/Categories/CategoryAdd.razor.cs
@@ -0,0 +1,37 @@
+﻿using Meowv.Blog.Dto.Blog.Params;
+using Meowv.Blog.Response;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Meowv.Blog.Admin.Pages.Categories
+{
+    public partial class CategoryAdd
+    {
+        CreateCategoryInput input = new CreateCategoryInput();
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        public async Task HandleSubmit()
+        {
+            if (string.IsNullOrWhiteSpace(input.Name) || string.IsNullOrWhiteSpace(input.Alias))
+            {
+                return;
+            }
+
+            var json = JsonConvert.SerializeObject(input);
+
+            var response = await GetResultAsync<BlogResponse>("api/meowv/blog/category", json, HttpMethod.Post);
+            if (response.Success)
+            {
+                await Message.Success("Successful", 0.5);
+                NavigationManager.NavigateTo("/categories/list");
+            }
+            else
+            {
+                await Message.Error(response.Message);
+            }
+        }
+    }
+}
