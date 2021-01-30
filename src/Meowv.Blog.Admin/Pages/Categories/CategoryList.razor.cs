33da604b2dcac926c728319be3db0306b30eb450
阿星Plus <hackxing@foxmail.com>
2021-01-30 13:50:56 +0800

    编辑分类

--- src/Meowv.Blog.Admin/Pages/Categories/CategoryList.razor.cs
+++ src/Meowv.Blog.Admin/Pages/Categories/CategoryList.razor.cs
@@ -1,5 +1,8 @@
 ﻿using Meowv.Blog.Dto.Blog;
+using Meowv.Blog.Dto.Blog.Params;
 using Meowv.Blog.Response;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -9,6 +12,14 @@ namespace Meowv.Blog.Admin.Pages.Categories
     public partial class CategoryList
     {
         List<GetAdminCategoryDto> categories;
+
+        UpdateCategoryInput input = new UpdateCategoryInput();
+
+        bool visible = false;
+
+        string categoryId;
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
@@ -32,6 +43,39 @@ namespace Meowv.Blog.Admin.Pages.Categories
             else
             {
                 await Message.Error("删除失败");
+            }
+        }
+
+        private void Close() => visible = false;
+
+        private void Open(GetAdminCategoryDto dto)
+        {
+            categoryId = dto.Id;
+            input.Name = dto.Name;
+            input.Alias = dto.Alias;
+
+            visible = true;
+        }
+
+        public async Task HandleSubmit()
+        {
+            if (string.IsNullOrWhiteSpace(input.Name) || string.IsNullOrWhiteSpace(input.Alias))
+            {
+                return;
+            }
+
+            var json = JsonConvert.SerializeObject(input);
+
+            var response = await GetResultAsync<BlogResponse>($"api/meowv/blog/category/{categoryId}", json, HttpMethod.Put);
+            if (response.Success)
+            {
+                Close();
+                await Message.Success("Successful", 0.5);
+                categories = await GetCategoryListAsync();
+            }
+            else
+            {
+                await Message.Error(response.Message);
             }
         }
     }
