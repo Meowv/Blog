20a80e3568ffce288ba6b1d8cc053008cdc65810
阿星Plus <hackxing@foxmail.com>
2021-02-01 22:45:15 +0800

    文章数据处理

--- src/Meowv.Blog.Application/Blog/Impl/BlogService.cs
+++ src/Meowv.Blog.Application/Blog/Impl/BlogService.cs
@@ -3,6 +3,8 @@ using Meowv.Blog.Domain.Blog.Repositories;
 using Meowv.Blog.Response;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Meowv.Blog.Blog.Impl
@@ -44,5 +46,38 @@ namespace Meowv.Blog.Blog.Impl
             response.Result = new Tuple<int, int, int>(postCount.To<int>(), categoryCount.To<int>(), tagCount.To<int>());
             return response;
         }
+
+        public async Task<List<PostModel>> GetPostsAsync()
+        {
+            var posts = await _posts.GetListAsync();
+
+            return posts.OrderBy(x => x.CreatedAt).Select(x => new PostModel
+            {
+                Title = x.Title,
+                Author = x.Author,
+                Url = x.Url,
+                Markdown = x.Markdown,
+                Category = x.Category.Name,
+                Tag = x.Tags.Select(t => t.Name).ToList(),
+                CreatedAt = x.CreatedAt.ToLocalTime().ToString("yyyy-MM-dd HH:mm:dd")
+            }).ToList();
+        }
+    }
+
+    public class PostModel
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Url { get; set; }
+
+        public string Markdown { get; set; }
+
+        public string Category { get; set; }
+
+        public List<string> Tag { get; set; }
+
+        public string CreatedAt { get; set; }
     }
 }
